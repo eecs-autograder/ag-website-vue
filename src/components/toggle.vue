54aebd2134a5c2356bacc418a03886db20698fa1
Ashley Berg <ashberg@umich.edu>
2018-08-09 12:21:34 -0400

    Modified Toggle component to match that of the Toggle component in the Angular version of the Autograder website.

--- src/components/toggle.vue
+++ src/components/toggle.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>
-    <button type="button" v-on:click="_toggle">
-      <div v-if="is_on">On</div>
-      <div v-else>Off</div>
-    </button>
+  <div id="toggle-button-space">
+
+    <div v-if="is_on" class="active-option-style on-border">
+      <p> {{on_side_text}} </p>
+    </div>
+    <div v-else @click="_toggle()" class="inactive-option-style on-border cursor-pointer">
+      <p> {{on_side_text}} </p>
+    </div>
+
+    <div v-if="is_on" @click="_toggle()" class="inactive-option-style off-border">
+      <p> {{off_side_text}} </p>
+    </div>
+    <div v-else class="active-option-style off-border">
+      <p> {{off_side_text}} </p>
+    </div>
+
   </div>
 </template>
 
@@ -17,13 +28,23 @@ export default class Toggle extends Vue {
   @Prop({default: false, type: Boolean})
   value!: boolean;
 
+  @Prop({default: "On", type: String})
+  switch_is_on_text: string;
+
+  @Prop({default: "Off", type: String})
+  switch_is_off_text: string;
+
   // data must be public and be initialized.
   is_on: boolean = false;
+  on_side_text: string;
+  off_side_text: string;
 
   // Vue doesn't like mutable properties, so we copy the value
   // into a data member.
   created() {
     this.is_on = this.value;
+    this.on_side_text = this.switch_is_on_text;
+    this.off_side_text = this.switch_is_off_text;
   }
 
   private _toggle() {
@@ -37,7 +58,60 @@ export default class Toggle extends Vue {
 <style scoped lang="scss">
 @import '@/styles/colors.scss';
 
-button {
-  color: $color-aqua;
+#toggle-button-space {
+  height: 25px;
+  border-radius: 3px;
+  z-index: 4;
+  display: inline-block;
 }
+
+.active-option-style {
+  opacity: 1;
+  font-weight: normal;
+  color: white;
+  display: inline-block;
+  height: 25px;
+  z-index: 4;
+  box-shadow: 0px 1px 1px $shadow-color;
+  background-color: $medium-dark-blue;
+  color: white;
+}
+
+.inactive-option-style {
+  opacity: 0.7;
+  font-weight: normal;
+  color: black;
+  display: inline-block;
+  height: 25px;
+  z-index: 3;
+  box-shadow: inset 1px 1px 3px $shadow-color;
+  cursor: pointer;
+}
+
+.active-option-style p, .inactive-option-style p {
+  margin: 0px;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size: 13px;
+}
+
+.on-border {
+  border-radius: 3px 0 0 3px;
+}
+
+.off-border {
+  border-radius: 0px 3px 3px 0;
+}
+
+button {
+  color: white;
+  background-color: black;
+}
+
+p {
+  position: relative;
+  top: 52%;
+  transform: perspective(1px) translateY(-50%);
+}
+
 </style>
