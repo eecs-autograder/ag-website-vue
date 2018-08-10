2dacaf53236d4c6097f682424e3c33ee174af8d4
Ashley Berg <ashberg@umich.edu>
2018-08-10 11:15:21 -0400

    Add tests for ViewFile component in 'tests'.

--- tests/test_hello_world.ts
+++ tests/test_hello_world.ts
@@ -1,11 +1,1 @@
-import HelloWorld from '@/components/hello_world.vue';
 
-import { mount } from '@vue/test-utils';
-
-describe('HelloWorld.vue', () => {
-    test('renders props.msg when passed', () => {
-        // const msg = 'new message';
-        const wrapper = mount(HelloWorld);
-
-    });
-});
