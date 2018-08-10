46e5568587866a629d9cb91405e2c5fafe1a797d
Ashley Berg <ashberg@umich.edu>
2018-08-10 14:46:13 -0400

    Deleted an unused css class in the Toggle component.

--- src/components/toggle.vue
+++ src/components/toggle.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="toggle-button-space">
-
     <div v-if="is_on" class="active-option-style on-border">
       <p> {{on_side_text}} </p>
     </div>
@@ -14,7 +13,6 @@
     <div v-else class="active-option-style off-border">
       <p> {{off_side_text}} </p>
     </div>
-
   </div>
 </template>
 
@@ -23,8 +21,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Toggle extends Vue {
-  // Input property. The ! is to make typescript not complain about this not being initialized
-  // in the ctor.
+
   @Prop({default: false, type: Boolean})
   value!: boolean;
 
@@ -34,13 +31,10 @@ export default class Toggle extends Vue {
   @Prop({default: "Off", type: String})
   switch_is_off_text: string;
 
-  // data must be public and be initialized.
   is_on: boolean = false;
   on_side_text: string = "On";
   off_side_text: string = "Off";
 
-  // Vue doesn't like mutable properties, so we copy the value
-  // into a data member.
   created() {
     this.is_on = this.value;
     this.on_side_text = this.switch_is_on_text;
@@ -54,45 +48,46 @@ export default class Toggle extends Vue {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import '@/styles/colors.scss';
 
 #toggle-button-space {
-  height: 25px;
   border-radius: 3px;
-  z-index: 4;
   display: inline-block;
+  height: 25px;
+  z-index: 4;
 }
 
 .active-option-style {
-  opacity: 1;
-  font-weight: normal;
+  background-color: $medium-dark-blue;
+  box-shadow: 0px 1px 1px $shadow-color;
   color: white;
   display: inline-block;
+  font-weight: normal;
   height: 25px;
+  opacity: 1;
   z-index: 4;
-  box-shadow: 0px 1px 1px $shadow-color;
-  background-color: $medium-dark-blue;
-  color: white;
 }
 
 .inactive-option-style {
-  opacity: 0.7;
-  font-weight: normal;
-  color: black;
-  display: inline-block;
-  height: 25px;
-  z-index: 3;
   box-shadow: inset 1px 1px 3px $shadow-color;
+  color: black;
   cursor: pointer;
+  display: inline-block;
+  font-weight: normal;
+  height: 25px;
+  opacity: 0.7;
+  z-index: 3;
 }
 
 .active-option-style p, .inactive-option-style p {
+  font-size: 13px;
   margin: 0px;
   padding-left: 10px;
   padding-right: 10px;
-  font-size: 13px;
+  position: relative;
+  top: 52%;
+  transform: perspective(1px) translateY(-50%);
 }
 
 .on-border {
@@ -102,16 +97,4 @@ export default class Toggle extends Vue {
 .off-border {
   border-radius: 0px 3px 3px 0;
 }
-
-button {
-  color: white;
-  background-color: black;
-}
-
-p {
-  position: relative;
-  top: 52%;
-  transform: perspective(1px) translateY(-50%);
-}
-
 </style>
