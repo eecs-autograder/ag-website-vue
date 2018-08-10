b22ce58ff1b5d5bb16734547514c020865184427
Ashley Berg <ashberg@umich.edu>
2018-08-10 11:15:21 -0400

    Add tests for ViewFile component in 'tests'.

--- tests/test_hello_world.ts
+++ tests/test_hello_world.ts
@@ -8,5 +8,32 @@ describe('HelloWorld.vue', () => {
         const wrapper = mount(HelloWorld, {
             propsData: { msg }
         });
+<<<<<<< 0dfcaec28d0e0e1fbe760ff90424c8c9d15d0de3
+=======
+
+        expect(wrapper.text()).toContain(msg);
+
+        // expect(wrapper.vm.$data.switchy).toEqual(true);
+        // // To select a component (css selectors won't work), add the "ref" attribute to
+        // // the html instance of the component you want.
+        let toggle = wrapper.find({ref: 'switchy_toggle'});
+        expect(toggle.vm.$data.is_on).toEqual(true);
+
+        let viewfile = wrapper.find({ref: 'viewing_file'});
+        expect(viewfile.vm.$data.filename).toEqual('ke$ha.cpp');
+
+        //
+        // let button = toggle.find('button');
+        // button.trigger('click');
+        //
+        // expect(wrapper.vm.$data.switchy).toEqual(false);
+        // expect(toggle.vm.$data.is_on).toEqual(false);
+
+        // let button = toggle.find('button');
+        // button.trigger('click');
+        //
+        // expect(wrapper.vm.$data.switchy).toEqual(false);
+        // expect(toggle.vm.$data.is_on).toEqual(false);
+>>>>>>> Add tests for ViewFile component in 'tests'.
     });
 });
---
+++ tests/test_view_file.ts
@@ -0,0 +1,45 @@
+import ViewFile from '@/components/view_file.vue';
+import { mount } from '@vue/test-utils';
+
+describe('ViewFile.vue', () => {
+    test('Data set to default values when props are not specified by parent', () => {
+        const wrapper = mount(ViewFile);
+        const vm = wrapper.vm;
+        expect(vm.$data.filename).toBe("name");
+        expect(vm.$data.file_content).toBe("content");
+
+    });
+
+    test('Data set to values passed in by parent', () => {
+        const filename = 'ke$ha_file.cpp';
+        const content = 'blah\nblah\nblah';
+        const wrapper = mount(ViewFile, {
+            propsData: {
+                incoming_filename: filename,
+                incoming_file_content: content
+            }
+        });
+
+        const vm = wrapper.vm;
+        expect(vm.$data.filename).toBe(filename);
+        expect(vm.$data.file_content).toBe(content);
+
+    });
+
+    test('The amount of line numbers corresponds to the number of lines with content', () => {
+        const content = 'blah\nblah\nblah';
+        const wrapper = mount(ViewFile, {
+            propsData: {
+                incoming_file_content: content
+            }
+        });
+        const vm = wrapper.vm;
+        expect(vm.$data.filename).toBe("name");
+        expect(vm.$data.file_content).toBe(content);
+        expect(wrapper.html()).toContain('<div class="line-number"> 1 </div>');
+        expect(wrapper.html()).toContain('<div class="line-number"> 2 </div>');
+        expect(wrapper.html()).toContain('<div class="line-number"> 3 </div>');
+        expect(wrapper.findAll('.line-number')).toHaveLength(3);
+    });
+
+});
