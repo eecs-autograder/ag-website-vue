638fd80ebcddecd758086d5af54e61b29bd737cd
Ashley Berg <ashberg@umich.edu>
2018-08-10 14:36:40 -0400

    Added tests for Toggle component in 'tests'.

--- tests/test_hello_world.ts
+++ tests/test_hello_world.ts
@@ -8,19 +8,5 @@ describe('HelloWorld.vue', () => {
         const wrapper = mount(HelloWorld, {
             propsData: { msg }
         });
-
-        expect(wrapper.text()).toMatch(msg);
-
-        expect(wrapper.vm.$data.switchy).toEqual(true);
-        // To select a component (css selectors won't work), add the "ref" attribute to
-        // the html instance of the component you want.
-        let toggle = wrapper.find({ref: 'switchy_toggle'});
-        expect(toggle.vm.$data.is_on).toEqual(true);
-
-        let button = toggle.find('button');
-        button.trigger('click');
-
-        expect(wrapper.vm.$data.switchy).toEqual(false);
-        expect(toggle.vm.$data.is_on).toEqual(false);
     });
 });
