62f0f8078217b517667a797f5aaabdb0c242c4ee
James P <jameslp@umich.edu>
2018-08-27 17:00:21 -0400

    Added @Watch to 'value' prop in toggle and tabs.

--- tests/test_tabs.ts
+++ tests/test_tabs.ts
@@ -110,7 +110,7 @@ describe('Tabs tests', () => {
         const wrapper = mount(component);
         const tabs = wrapper.find({ref: 'tabs'});
 
-        expect(tabs.vm.$props.value).toEqual(0);
+        expect(tabs.vm.$data.active_tab_index).toEqual(0);
 
         let active_body = tabs.find({ref: 'active-tab-body'});
         expect(active_body.text()).toEqual('Tab 1 body');
@@ -124,12 +124,113 @@ describe('Tabs tests', () => {
         expect(active_body.text()).toEqual('Tab 2 body');
     });
 
-    test('Custom click handler on <tab>', async () => {
-        fail();
+    test.only('Custom click handler on <tab>', async () => {
+        const component = {
+            template:  `<tabs ref="tabs">
+  <tab>
+    <template slot="header">
+      Tab 1
+    </template>
+    <template slot="body">
+     Tab 1 body
+    </template>
+  </tab>
+  <tab ref="tab_2" @click="datum += 1">
+    <template slot="header">
+      Tab 2
+    </template>
+    <template slot="body">
+      Tab 2 body
+    </template>
+  </tab>
+</tabs>`,
+            components: {
+                'tab': Tab,
+                'tabs': Tabs
+            },
+            data: () => {
+                return {
+                    datum: 0
+                };
+            }
+        };
+
+        const wrapper = mount(component);
+        const tabs = wrapper.find({ref: 'tabs'});
+
+        expect(wrapper.vm.$data.datum).toEqual(0);
+        expect(tabs.vm.$data.active_tab_index).toEqual(0);
+
+        let active_body = tabs.find({ref: 'active-tab-body'});
+        expect(active_body.text()).toEqual('Tab 1 body');
+
+        let tab_2 = tabs.find({ref: 'tab_2'});
+        tab_2.trigger('click');
+
+        expect(wrapper.vm.$data.datum).toEqual(1);
+        expect(tabs.vm.$data.active_tab_index).toEqual(1);
+
+        active_body = tabs.find({ref: 'active-tab-body'});
+        expect(active_body.text()).toEqual('Tab 2 body');
+
+        tab_2.trigger('click');
+        expect(wrapper.vm.$data.datum).toEqual(2);
     });
 
-    test('Tab v-model change', async () => {
-        fail();
+    test.only('Tab v-model binding', async () => {
+        const component = {
+            template:  `<tabs ref="tabs" v-model="current_tab">
+  <tab ref="tab_1">
+    <template slot="header">
+      Tab 1
+    </template>
+    <template slot="body">
+     Tab 1 body
+    </template>
+  </tab>
+  <tab>
+    <template slot="header">
+      Tab 2
+    </template>
+    <template slot="body">
+      Tab 2 body
+    </template>
+  </tab>
+</tabs>`,
+            components: {
+                'tab': Tab,
+                'tabs': Tabs
+            },
+            data: () => {
+                return {
+                    current_tab: 0
+                };
+            }
+        };
+
+        const wrapper = mount(component);
+        const tabs = wrapper.find({ref: 'tabs'});
+
+        expect(tabs.vm.$data.active_tab_index).toEqual(0);
+
+        let active_body = tabs.find({ref: 'active-tab-body'});
+        expect(active_body.text()).toEqual('Tab 1 body');
+
+        // Set current_tab, active tab should change
+        wrapper.setData({current_tab: 1});
+
+        expect(tabs.vm.$data.active_tab_index).toEqual(1);
+
+        active_body = tabs.find({ref: 'active-tab-body'});
+        expect(active_body.text()).toEqual('Tab 2 body');
+
+        // Click on inactive tab, current_tab should change
+        let tab_1 = tabs.find({ref: 'tab_1'});
+        tab_1.trigger('click');
+
+        expect(active_body.text()).toEqual('Tab 1 body');
+        expect(tabs.vm.$data.active_tab_index).toEqual(0);
+        expect(wrapper.vm.$data.current_tab).toEqual(0);
     });
 
     test('Active index off end selects last tab', async () => {
