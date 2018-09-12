db9ce160fc6e6b0e28720c80e943b6d98fa2c297
Ashley Berg <ashberg@umich.edu>
2018-09-12 13:00:15 -0400

    Created a template to display a dropdown menu on an element replacing the header slot element. Attached event handlers to an encompassing 'dropdown' parent element and the element encircling the slot element in order to reduce duplicated code when the component is reused. The element passed into the slot can override these behaviors when certain events occur by attaching its own event handlers.

--- src/components/dropdown.vue
+++ src/components/dropdown.vue
@@ -1,30 +1,31 @@
 <template>
-  <div class="dropdown-container">
-    <div class="dropdown" tabindex="1"
-      @blur="hide_the_dropdown_menu()">
-      <div id="dropdown-top"
-         @click="toggle_dropdown_menu()">
-        <p> {{chosen_item}} </p>
-        <i class="fas fa-angle-down fa-lg arrow-down"> </i>
-      </div>
-      <div class="dropdown-content">
+
+  <div class="dropdown"
+       tabindex="1"
+       @keydown="move_highlighted($event)"
+       @blur="hide_the_dropdown_menu">
+
+    <div @click="toggle_dropdown_menu">
+      <slot name="drop_top"> </slot>
+    </div>
+
+    <div class="dropdown-container">
+      <div class="dropdown-content" :style="content_styling">
         <a v-for="(item, index) of items"
-           @click="choose_item_from_dropdown_menu(item, index)"
-           :id="index === highlighted_index ? 'highlight' : ''">
-          {{item}}
+        @click="choose_item_from_dropdown_menu(item, index)"
+        :id="index === highlighted_index ? 'highlight' : ''">
+        {{item}}
         </a>
       </div>
     </div>
   </div>
+
 </template>
 
 <script lang="ts">
-  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
-  @Component({
-    components: { }
-  })
-
+  @Component
   export default class Dropdown extends Vue {
 
     @Prop({required: true, type: Array})
@@ -33,50 +34,135 @@
     @Prop({default: "", type: String})
     incoming_chosen_item!: string;
 
-    dropdown_is_visible = false;
-    items: string[] = [];
+    @Prop({default: "", type: String})
+    dropdown_content_min_width!: string;
+
+    @Prop({default: "", type: String})
+    dropdown_content_max_width!: string;
 
     chosen_item: string = "";
-
     highlighted_index = 0;
+    items: string[] = [];
+    content_min_width = "";
+    content_max_width = "";
+    content_styling = {};
+    is_open_ = false;
 
     created() {
       this.items = this.incoming_items;
       this.chosen_item = this.incoming_chosen_item === ""
         ? this.items[0] : this.incoming_chosen_item;
-      this.highlighted_index = this.items.findIndex(this.find_chosen_item);
+      this.highlighted_index = this.items.findIndex(item => item === this.chosen_item);
+      this.content_min_width = this.dropdown_content_min_width;
+      this.content_max_width = this.dropdown_content_max_width;
+      this.add_styling();
+    }
+
+    get is_open() {
+      return this.is_open_;
+    }
+
+    add_styling() {
+      if (this.content_min_width !== "" && this.content_max_width !== "") {
+        this.content_styling = {
+          'minWidth' : this.content_min_width,
+          'maxWidth' : this.content_max_width
+        };
+      }
+      else if (this.content_min_width === "" && this.content_max_width !== "") {
+        this.content_styling = {
+          'maxWidth' : this.content_max_width
+        };
+      }
+      else if (this.content_min_width !== "" && this.content_max_width === "") {
+        this.content_styling = {
+          'minWidth' : this.content_min_width
+        };
+      }
+      else {
+        this.content_styling = { };
+      }
+    }
+
+    mounted() {
+      if (this.is_open) {
+        this.show_the_dropdown_menu();
+      }
+      else {
+        this.hide_the_dropdown_menu();
+      }
     }
 
     find_chosen_item(item: string) {
-      console.log(item);
-      console.log(item === this.chosen_item);
       return item === this.chosen_item;
     }
 
     show_the_dropdown_menu() {
       let dropdown_menu = <HTMLElement> this.$el.getElementsByClassName("dropdown-content")[0];
       dropdown_menu.style.display = "block";
-      this.dropdown_is_visible = true;
+      this.is_open_ = true;
     }
+
     hide_the_dropdown_menu() {
       let dropdown_menu = <HTMLElement> this.$el.getElementsByClassName("dropdown-content")[0];
       dropdown_menu.style.display = "none";
-      this.dropdown_is_visible = false;
-      console.log("Closed it");
+      this.is_open_ = false;
     }
 
     choose_item_from_dropdown_menu(item_selected: string, index: number) {
-      this.chosen_item = item_selected;
-      this.highlighted_index = index;
+      if (item_selected) {
+        this.chosen_item = item_selected;
+        this.highlighted_index = index;
+        this.$emit("update_item_selected", item_selected);
+      }
       this.hide_the_dropdown_menu();
     }
 
     toggle_dropdown_menu() {
-      if (this.dropdown_is_visible) {
+      if (this.is_open) {
+        this.is_open_ = false;
         this.hide_the_dropdown_menu();
       }
       else {
+        this.is_open_ = true;
         this.show_the_dropdown_menu();
+      }
+    }
+
+    move_highlighted(event: KeyboardEvent) {
+      event.preventDefault();
+      event.stopPropagation();
+      if (event.code === "Enter") {
+        event.preventDefault();
+        event.stopPropagation();
+        if (this.highlighted_index !== -1 && this.is_open) {
+          this.choose_item_from_dropdown_menu(
+            this.items[this.highlighted_index], this.highlighted_index
+          );
+        }
+      }
+      else {
+        if (!this.is_open) {
+          this.show_the_dropdown_menu();
+        }
+        if (event.code === 'ArrowDown') {
+          if (this.highlighted_index < this.items.length - 1) {
+            this.highlighted_index += 1;
+          }
+          console.log(this.highlighted_index);
+        }
+        else if (event.code === 'ArrowUp') {
+          if (this.highlighted_index !== -1) {
+            this.highlighted_index -= 1;
+            if (this.highlighted_index < 0) {
+              this.highlighted_index = 0;
+              console.log(this.highlighted_index);
+            }
+          }
+        }
+        else if (event.code === 'Escape') {
+          this.highlighted_index = 0;
+        }
       }
     }
 
@@ -87,22 +173,27 @@
 <style scoped lang="scss">
   @import '@/styles/colors.scss';
 
+  .dropdown:focus {
+    outline: none;
+  }
+
   .dropdown {
     display: inline-block;
     position: relative;
   }
 
-  .dropdown:focus {
-    outline:none;
+  .dropdown-outer:focus {
+    outline: none;
   }
 
   .dropdown-content {
     background-color: white;
+    border-radius: 5px;
     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     display: none;
-    margin-top: 1px;
-    position: absolute;
+    margin-top: 10px;
     width: 100%;
+    position: absolute;
     z-index: 1;
   }
   .dropdown-content a {
@@ -113,7 +204,7 @@
     font-family: "Helvetica Neue", Helvetica, sans-serif;
     margin: 0;
     min-height: 13px;
-    padding: 8px 10px;
+    padding: 10px 15px;
     text-decoration: none;
   }
   .dropdown-content a:first-child {
@@ -131,40 +222,6 @@
   }
   #highlight {
     background-color: hsl(210, 13%, 80%);
-  }
-
-  #dropdown-top {
-    background-color: hsl(210, 13%, 90%);
-    border: 2px solid hsl(210, 13%, 90%);
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    color: black;
-    font-family: "Helvetica Neue", Helvetica, sans-serif;
-    font-size: 16px;
-    margin: 0;
-    padding: 6px 10px;
-    min-width: 200px;
-  }
-
-  #dropdown-top p {
-    margin: 0;
-    padding: 2px;
-  }
-
-  #dropdown-top:hover {
-    background-color: hsl(210, 12%, 93%);
-    border-color: hsl(210, 12%, 93%);
-    color: black;
-  }
-
-  .arrow-down {
-    position: absolute;
-    right: 22px;
-    top: 13px;
-    color: hsl(210, 12%, 40%);
-  }
-
-  .arrow-down:hover {
-    color: hsl(210, 12%, 30%);
   }
 
 </style>
