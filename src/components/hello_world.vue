a74ddb366c373d6cdb09f770abcaf5691b14bcb8
Ashley Berg <ashberg@umich.edu>
2018-09-12 13:00:15 -0400

    Created a template to display a dropdown menu on an element replacing the header slot element. Attached event handlers to an encompassing 'dropdown' parent element and the element encircling the slot element in order to reduce duplicated code when the component is reused. The element passed into the slot can override these behaviors when certain events occur by attaching its own event handlers.

--- src/components/hello_world.vue
+++ src/components/hello_world.vue
@@ -39,6 +39,21 @@ import TooltipDemo from '@/demos/tooltip_demo.vue';
   }
 })
 export default class HelloWorld extends Vue {
+  @Prop()
+  msg!: string;
+
+  selected_choice = "5 Passengers";
+
+  menu_items = ["1 Passenger", "2 Passengers", "3 Passengers", "4 Passengers", "5 Passengers",
+                "6 Passengers", "7 Passengers", "8 Passengers", "9 Passengers"];
+
+  add_item(item: string) {
+    this.selected_choice = item;
+    this.chosen_items.push(item);
+  }
+
+
+  chosen_items: string[] = [];
 
 }
 </script>
@@ -46,5 +61,59 @@ export default class HelloWorld extends Vue {
 <style scoped lang="scss">
   @import '@/styles/colors.scss';
 
+  #drop-top {
+    background-color: hsl(210, 13%, 95%);
+    border: 2px solid hsl(210, 13%, 95%);
+    border-radius: 5px;
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    color: black;
+    font-family: "Helvetica Neue", Helvetica, sans-serif;
+    font-size: 16px;
+    height: 24px;
+    margin: 0;
+    padding: 6px 55px 6px 10px;
+  }
+
+  #drop-top p {
+    padding: 2px;
+    margin: 0;
+  }
+
+  .arrow-down {
+    position: absolute;
+    right: 25px;
+    top: 14px;
+  }
+
+  #drop-top:focus {
+    outline: none;
+  }
+
+  #drop-top:hover {
+    background-color: hsl(210, 12%, 93%);
+    border-color: hsl(210, 12%, 93%);
+    color: black;
+  }
+
+  .drop-button {
+    outline: none;
+    color: white;
+  }
+
+  .drop-button {
+    padding: 20px;
+    background-color: cornflowerblue;
+    color: black;
+    font-size: 20px;
+    border: none;
+    cursor: pointer;
+    margin: 0;
+    width: 200px;
+    text-align: center;
+  }
+
+  .drop-button:focus {
+    outline: 2px solid red;
+  }
 
 </style>
