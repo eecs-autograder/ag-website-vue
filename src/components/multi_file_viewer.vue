d3c1f650825e36c8f2d895387dbee2260e0e5d12
Ashley Berg <ashberg@umich.edu>
2018-09-25 20:58:01 -0400

    Removed blank lines and swiss army knife functions from multi_file_viewer_demo and multi_file_viewer tests. Removed colors that are not currently being used from the colors.scss file.

---
+++ src/components/multi_file_viewer.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="multi-file-viewer">
+    <tabs ref="multi-viewing-tabs"
+          v-model="active_tab_index"
+          tab_active_class="gray-white-theme-active"
+          tab_inactive_class="gray-white-theme-inactive">
+       <tab ref="single-tab"
+            v-for="(filename, index) of Array.from(files_currently_viewing)"
+            @click="update_viewing(index)">
+
+         <template slot="header">
+           <div class="tab-header">
+             <p class="tab-label"> {{ filename }} </p>
+             <i class="fas fa-times close-x"
+                @click="$event.stopPropagation(); remove_from_viewing(filename, index)"></i>
+           </div>
+         </template>
+
+         <template slot="body">
+           <div class="tab-body">
+
+             <view-file
+               ref="view_file_component"
+               :incoming_filename="filename"
+               :incoming_file_contents="get_contents(filename)"
+               :incoming_height_specifications="scrollable_height">
+             </view-file>
+
+           </div>
+         </template>
+
+       </tab>
+    </tabs>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  import Tab from '@/components/tabs/tab.vue';
+  import Tabs from '@/components/tabs/tabs.vue';
+  import ViewFile from '@/components/view_file.vue';
+
+  @Component({
+    components: { Tab, Tabs, ViewFile }
+  })
+  export default class MultiFileViewer extends Vue {
+
+    @Prop({default: () => {}, type: Object})
+    height_of_view_file!: object;
+
+    file_names_and_content = new Map<string, string | undefined>();
+    files_currently_viewing: string[] = [];
+    active_tab_index = -1;
+    scrollable_height: object = {};
+
+    created() {
+      this.scrollable_height = this.height_of_view_file;
+    }
+
+    get_contents(filename: string) {
+      return this.file_names_and_content.get(filename);
+    }
+
+    update_viewing(index: number) {
+      this.active_tab_index = index;
+    }
+
+    add_to_viewing(filename: string, file_contents: string | undefined) {
+      if (!this.file_names_and_content.has(filename)) {
+        let names_and_content_copy = new Map(this.file_names_and_content);
+        let currently_viewing_copy = this.files_currently_viewing;
+        names_and_content_copy.set(filename, file_contents);
+        currently_viewing_copy.push(filename);
+        this.file_names_and_content = names_and_content_copy;
+        this.files_currently_viewing = currently_viewing_copy;
+        this.active_tab_index = this.files_currently_viewing.length - 1;
+      }
+    }
+
+    remove_from_viewing(filename_to_delete: string, tab_index: number) {
+      let names_and_content_copy = new Map(this.file_names_and_content);
+      let currently_viewing_copy = this.files_currently_viewing;
+      let old_size = this.file_names_and_content.size;
+      let old_active_index = this.active_tab_index;
+      let is_rightmost_tab = (old_size - 1) === old_active_index;
+
+      names_and_content_copy.delete(filename_to_delete);
+      currently_viewing_copy.splice(tab_index, 1);
+      this.file_names_and_content = names_and_content_copy;
+      this.files_currently_viewing = currently_viewing_copy;
+      if (tab_index < this.active_tab_index && !is_rightmost_tab) {
+        this.active_tab_index -= 1;
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/colors.scss';
+
+.close-x {
+  color: hsl(0.61, 89%, 71%);
+  cursor: pointer;
+  display: inline-block;
+  margin-bottom: 0;
+  position: absolute;
+  right: 15px;
+  top: 12px;
+}
+
+.close-x:hover {
+  color: hsl(0.61, 85%, 60%);
+}
+
+.tab-body {
+  background-color: white;
+  border: 2px solid hsl(210, 11%, 90%);
+  bottom: 0;
+  color: #24292e;
+  min-width: 800px;
+  position: relative;
+}
+
+.tab-header {
+  margin: 0;
+}
+
+.tab-label {
+  display: inline-block;
+  font-family: "Helvetica Neue", Helvetica;
+  margin: 0;
+  padding-right: 25px;
+}
+
+</style>
---
+++ src/demos/multi_file_viewer_demo.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="multi-file-viewer-demo">
+
+    <div>
+      <p>
+        <b>The height of the multi_file_viewer is determined by the view_files
+           inside of it. The 'height_of_view_file' variable specifies the height
+           in this example of the m.f.v </b>
+      </p>
+      <p class="btn fruits" @click="open_file('Apple')"> Add A </p>
+      <p class="btn fruits" @click="open_file('Banana')"> Add B </p>
+      <p class="btn fruits" @click="open_file('Cantaloupe')"> Add C </p>
+      <p class="btn fruits" @click="open_file('Dragonfruit')"> Add D </p>
+      <p class="btn fruits" @click="open_file('Grapes')"> Add E </p>
+      <div class="mvf-container">
+        <multi-file-viewer
+          ref="multi_file"
+          :height_of_view_file="height_of_view_file">
+        </multi-file-viewer>
+      </div>
+    </div>
+
+    <div>
+      <p>
+        <b>
+          In this example of the multi-file-viewer the view_files
+          inside of it resort to their default height.
+        </b>
+      </p>
+      <p class="btn letters" @click="open_file2('A')"> Add A </p>
+      <p class="btn letters" @click="open_file2('B')"> Add B </p>
+      <p class="btn letters" @click="open_file2('C')"> Add C </p>
+      <div class="mvf-container">
+        <multi-file-viewer
+          ref="multi_file2">
+        </multi-file-viewer>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+
+  import MultiFileViewer from '@/components/multi_file_viewer.vue';
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component({
+    components: { MultiFileViewer }
+  })
+  export default class MultiFileViewerDemo extends Vue {
+
+    files_and_content = new Map<string, string>([
+        ['A', 'Aurora\nAurora'],
+        ['B', 'Belle'],
+        ['C', 'Cinderella\nCinderella\nCinderella'],
+        ['Apple', 'Apples:\n\tRed\n\tGreen\n\tYellow'],
+        ['Banana', 'Beetlejuice\nBeetlejuice\nBeetlejuice'],
+        ['Cantaloupe', 'Cantaloupe'],
+        ['Dragonfruit', 'Dragonfruit\nDragonfruit\nDragonfruit\nDragonfruit'],
+        ['Grapes', 'Green Grapes\nPurple Grapes']
+    ]);
+
+    open_file(file_in: string) {
+      let mfv = <MultiFileViewer> this.$refs.multi_file;
+      mfv.add_to_viewing(file_in, this.files_and_content.get(file_in));
+    }
+
+    open_file2(file_in: string) {
+      let mfv = <MultiFileViewer> this.$refs.multi_file;
+      mfv.add_to_viewing(file_in, this.files_and_content.get(file_in));
+    }
+
+    height_of_view_file = {
+      "height": "585px"
+    };
+
+  }
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/colors.scss';
+
+.btn {
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  display: inline-block;
+  margin-right: 10px;
+  padding: 20px;
+}
+
+.mvf-container {
+  width: 90%;
+}
+
+.fruits {
+  background-color: $medium-dark-blue;
+}
+
+.letters {
+  background-color: mediumpurple;
+}
+
+.multi-file-viewer-demo {
+  padding: 20px 0;
+}
+
+</style>
