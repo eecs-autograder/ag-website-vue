676c9d6d4120304058c98245e5686f82da4c4645
James P <jameslp@umich.edu>
2018-10-02 17:40:15 -0400

    Removed some extra checks in multi file viewer tests. Also added some await vm.() calls before DOM inspection checks.

--- tests/test_multi_file_viewer.ts
+++ tests/test_multi_file_viewer.ts
@@ -44,66 +44,58 @@ describe('MultiFileViewer.vue', () => {
         );
     });
 
-    test('When a file is added it becomes the active file/tab ', () => {
+    test('When a file is added it becomes the active file/tab ', async () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
+        await multi_file_viewer.$nextTick();
 
-        let view_file_component = mvf.find({ref: 'view_file_component'});
+        let view_file_component = wrapper.find({ref: 'view_file_component'});
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
         expect(view_file_component.text()).toContain('Kiwi Body');
 
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
-        view_file_component = mvf.find({ref: 'view_file_component'});
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(2);
         expect(view_file_component.text()).toContain('Lime Body');
 
         multi_file_viewer.add_to_viewing('Mango', 'Mango Body');
-        view_file_component = mvf.find({ref: 'view_file_component'});
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(2);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(3);
         expect(view_file_component.text()).toContain('Mango Body');
 
         multi_file_viewer.add_to_viewing('Nectarine', 'Nectarine Body');
-        view_file_component = mvf.find({ref: 'view_file_component'});
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(4);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(3);
         expect(view_file_component.text()).toContain('Nectarine Body');
     });
 
-    test('Clicking on a tab makes it the active tab', () => {
+    test('Clicking on a tab makes it the active tab', async () => {
 
-        const wrapper = mount(WrapperComponent);
-        let mvf = wrapper.find({ref: 'multi_file'});
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
-
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
-
-        mvf = wrapper.find({ref: 'multi_file'});
-
         multi_file_viewer.add_to_viewing('Mango', 'Mango Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(2);
-
-        mvf = wrapper.find({ref: 'multi_file'});
-
         multi_file_viewer.add_to_viewing('Nectarine', 'Nectarine Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(3);
 
-        mvf = wrapper.find({ref: 'multi_file'});
+        await multi_file_viewer.$nextTick();
 
-        let view_file_component = mvf.find({ref: 'view_file_component'});
+        let view_file_component = wrapper.find({ref: 'view_file_component'});
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(4);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(3);
         expect(view_file_component.text()).toContain('Nectarine Body');
@@ -111,54 +103,61 @@ describe('MultiFileViewer.vue', () => {
         let first_tab = wrapper.findAll('.tab-label').at(0);
         first_tab.trigger('click');
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
+        expect(view_file_component.text()).toContain('Kiwi Body');
 
         let second_tab = wrapper.findAll('.tab-label').at(1);
         second_tab.trigger('click');
         expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
+        expect(view_file_component.text()).toContain('Lime Body');
     });
 
-    test('Only one copy of a file and its contents can exist at once in the mfv', () => {
+    test('Only one copy of a file and its contents can exist at once in the mfv', async () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
+        await multi_file_viewer.$nextTick();
 
-        let view_file_component = mvf.find({ref: 'view_file_component'});
+        let view_file_component = wrapper.find({ref: 'view_file_component'});
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
         expect(view_file_component.text()).toContain('Kiwi Body');
 
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
+        await multi_file_viewer.$nextTick();
 
-        view_file_component = mvf.find({ref: 'view_file_component'});
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(2);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
         expect(view_file_component.text()).toContain('Lime Body');
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
-        view_file_component = mvf.find({ref: 'view_file_component'});
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(2);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
         expect(view_file_component.text()).toContain('Lime Body');
     });
 
-    test('When leftmost tab is active & gets deleted, right neighbor becomes active', () => {
+    test('When leftmost tab is active & gets deleted, right neighbor becomes active', async () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
+        await multi_file_viewer.$nextTick();
+
+        let view_file_component = wrapper.find({ref: 'view_file_component'});
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
-        let view_file_component = mvf.find({ref: 'view_file_component'});
         expect(view_file_component.text()).toContain('Kiwi Body');
 
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(2);
         expect(view_file_component.text()).toContain('Lime Body');
@@ -170,22 +169,25 @@ describe('MultiFileViewer.vue', () => {
         let leftmost_tab_close_x = wrapper.findAll('.close-x');
         leftmost_tab_close_x.trigger('click');
 
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
         expect(view_file_component.text()).toContain('Lime Body');
     });
 
-    test('When the only tab gets deleted, the active_tab_index becomes negative', () => {
+    test('When the only tab gets deleted, the active_tab_index becomes negative', async () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
 
-        let view_file_component = mvf.find({ref: 'view_file_component'});
+        let view_file_component = wrapper.find({ref: 'view_file_component'});
         expect(view_file_component.text()).toContain('Kiwi Body');
 
         let leftmost_tab = wrapper.findAll('.tab-label').at(0);
@@ -199,26 +201,19 @@ describe('MultiFileViewer.vue', () => {
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
     });
 
-    test('When an active middle tab gets deleted, the right neighbor becomes active', () => {
+    test('When an active middle tab gets deleted, the right neighbor becomes active', async () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
-        expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
-
-        let view_file_component = mvf.find({ref: 'view_file_component'});
-        expect(view_file_component.text()).toContain('Kiwi Body');
-
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
-        expect(view_file_component.text()).toContain('Lime Body');
-
         multi_file_viewer.add_to_viewing('Mango', 'Mango Body');
-        expect(view_file_component.text()).toContain('Mango Body');
-
         multi_file_viewer.add_to_viewing('Nectarine', 'Nectarine Body');
+
+        await multi_file_viewer.$nextTick();
+
+        let view_file_component = wrapper.find({ref: 'view_file_component'});
         expect(view_file_component.text()).toContain('Nectarine Body');
         expect(multi_file_viewer.$data.active_tab_index).toEqual(3);
 
@@ -229,37 +224,26 @@ describe('MultiFileViewer.vue', () => {
         let second_tab_close_button = wrapper.findAll('.close-x').at(1);
         second_tab_close_button.trigger('click');
 
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
-        view_file_component = mvf.find({ref: 'view_file_component'});
         expect(view_file_component.text()).toContain('Mango Body');
     });
 
-    test('Deleting rightmost active tab causes left neighbor to become active', () => {
+    test('Deleting rightmost active tab causes left neighbor to become active', async () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
-        let view_file_component = mvf.find({ref: 'view_file_component'});
-        expect(view_file_component.text()).toContain('Kiwi Body');
-
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
-        expect(view_file_component.text()).toContain('Lime Body');
-
         multi_file_viewer.add_to_viewing('Mango', 'Mango Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(2);
-
         multi_file_viewer.add_to_viewing('Nectarine', 'Nectarine Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(3);
 
-        view_file_component = mvf.find({ref: 'view_file_component'});
+        await multi_file_viewer.$nextTick();
+
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(4);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(3);
-        expect(view_file_component.text()).toContain('Nectarine Body');
 
         let rightmost_tab_close_x = wrapper.findAll('.close-x').at(3);
         rightmost_tab_close_x.trigger('click');
@@ -270,26 +254,14 @@ describe('MultiFileViewer.vue', () => {
 
     test('Close middle tab when middle tab is not active', () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
-        expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
-        let view_file_component = mvf.find({ref: 'view_file_component'});
-        expect(view_file_component.text()).toContain('Kiwi Body');
-
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
-        expect(view_file_component.text()).toContain('Lime Body');
-
         multi_file_viewer.add_to_viewing('Mango', 'Mango Body');
-        expect(view_file_component.text()).toContain('Mango Body');
-        view_file_component = mvf.find({ref: 'view_file_component'});
-        expect(view_file_component.text()).toContain('Mango Body');
-
         multi_file_viewer.add_to_viewing('Nectarine', 'Nectarine Body');
-        expect(view_file_component.text()).toContain('Nectarine Body');
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(3);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(4);
 
@@ -303,21 +275,16 @@ describe('MultiFileViewer.vue', () => {
 
     test('Close leftmost tab when it is not active', () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
-        let view_file_component = mvf.find({ref: 'view_file_component'});
-        expect(view_file_component.text()).toContain('Kiwi Body');
 
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
-        expect(view_file_component.text()).toContain('Lime Body');
-
         multi_file_viewer.add_to_viewing('Mango', 'Mango Body');
-        expect(view_file_component.text()).toContain('Mango Body');
+
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(3);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(2);
 
@@ -326,26 +293,17 @@ describe('MultiFileViewer.vue', () => {
 
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(2);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
-        expect(view_file_component.text()).toContain('Mango Body');
     });
 
     test('Close rightmost tab when it is not active', () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         multi_file_viewer.add_to_viewing('Kiwi', 'Kiwi Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
-        expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
-        let view_file_component = mvf.find({ref: 'view_file_component'});
-        expect(view_file_component.text()).toContain('Kiwi Body');
-
         multi_file_viewer.add_to_viewing('Lime', 'Lime Body');
-        expect(view_file_component.text()).toContain('Lime Body');
-
         multi_file_viewer.add_to_viewing('Mango', 'Mango Body');
-        expect(view_file_component.text()).toContain('Mango Body');
+
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(3);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(2);
 
@@ -357,43 +315,26 @@ describe('MultiFileViewer.vue', () => {
 
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(2);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
-        expect(view_file_component.text()).toContain('Kiwi Body');
 
         let second_tab_close_x = wrapper.findAll('.close-x').at(1);
         second_tab_close_x.trigger('click');
 
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
         expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
-        expect(view_file_component.text()).toContain('Kiwi Body');
     });
 
 
     test('Close tab to left of active tab (active tab is not rightmost tab)', () => {
 
-        const wrapper = mount(WrapperComponent);
-        let multi_file_viewer = <MultiFileViewer> wrapper.find({ref: 'multi_file'}).vm;
-        let mvf = wrapper.find({ref: 'multi_file'});
+        const wrapper = mount(WrapperComponent).find({ref: 'multi_file'});
+        let multi_file_viewer = <MultiFileViewer> wrapper.vm;
 
         multi_file_viewer.add_to_viewing('Monday', 'Monday Body');
-
-        let view_file_component = mvf.find({ref: 'view_file_component'});
-        expect(view_file_component.text()).toContain('Monday Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(0);
-        expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(1);
-
         multi_file_viewer.add_to_viewing('Tuesday', 'Tuesday Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(1);
-        expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(2);
-
         multi_file_viewer.add_to_viewing('Wednesday', 'Wednesday Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(2);
-        expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(3);
-
         multi_file_viewer.add_to_viewing('Thursday', 'Thursday Body');
-        expect(multi_file_viewer.$data.active_tab_index).toEqual(3);
-        expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(4);
-
         multi_file_viewer.add_to_viewing('Friday', 'Friday Body');
+
         expect(multi_file_viewer.$data.active_tab_index).toEqual(4);
         expect(multi_file_viewer.$data.files_currently_viewing.length).toEqual(5);
 
