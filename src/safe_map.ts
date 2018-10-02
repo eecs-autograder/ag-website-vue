29f39fddeb0e6093ab3a0eb2ac0e2113cdc40856
Ashley Berg <ashberg@umich.edu>
2018-10-02 16:42:46 -0400

    Deleted the SafeMap constructor, Iterator interface, and Foreachable Interface. Made the my_map member variable private, and added the [Symbol.iterator]() method to the SafeMap class.

--- src/safe_map.ts
+++ src/safe_map.ts
@@ -1,64 +1,54 @@
-// All arguments had type any
-interface Iterator<T> {
-    next(value?: T): IteratorResult<T>;
-    return?(value?: T): IteratorResult<T>;
-    throw?(e?: T): IteratorResult<T>;
-}
-
-interface ForEachable<T> {
-    forEach(callbackfn: (value: T) => void): void;
-}
-
 export class SafeMapError extends Error {}
 
 export class SafeMap<K, V>  {
 
-    my_map = new Map<K, V>();
+    private _my_map = new Map<K, V>();
 
     get size() {
-        return this.my_map.size;
+        return this._my_map.size;
     }
 
-    constructor(iterable?: ForEachable<[K, V]>) { }
-
     clear() {
-        this.my_map.clear();
+        this._my_map.clear();
     }
 
     delete(key: K) {
-        this.my_map.delete(key);
+        this._my_map.delete(key);
     }
 
-    // I'm not sure what this_arg refers to? --> was of type any, replaced it with type object
-    for_each(callbackfn: (value: V, index: K, map: Map<K, V>) => void, this_arg?: object): void {
-        return this.my_map.forEach(callbackfn);
+    for_each(callbackfn: (value: V, index: K, map: Map<K, V>) => void): void {
+        return this._my_map.forEach(callbackfn);
     }
 
     get(key: K): V {
-        if (!this.my_map.has(key)) {
+        if (!this._my_map.has(key)) {
             throw new SafeMapError("Key: " + key + ", is not in the map!");
         }
-        return this.my_map.get(key)!;
+        return this._my_map.get(key)!;
     }
 
     has(key: K): boolean {
-        return this.my_map.has(key);
+        return this._my_map.has(key);
     }
 
     set(key: K, value: V): Map<K, V> {
-        return this.my_map.set(key, value);
+        return this._my_map.set(key, value);
     }
 
     entries(): Iterator<[K, V]> {
-        return this.my_map.entries();
+        return this._my_map.entries();
     }
 
     keys(): Iterator<K> {
-        return this.my_map.keys();
+        return this._my_map.keys();
     }
 
     values(): Iterator<V> {
-        return this.my_map.values();
+        return this._my_map.values();
+    }
+
+    [Symbol.iterator]() {
+        return this._my_map[Symbol.iterator]();
     }
 }
 
