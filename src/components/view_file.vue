be8e67e764cf9d0463dab542ca4c9a5e827661e2
Ashley Berg <ashberg@umich.edu>
2018-10-03 18:32:44 -0400

    Added examples of the view file component in view_file_demo.vue. Added a test to test_view_file.ts to make sure that if a custom height is passed in to the ViewFile component, the height is set to the custom height. Added another test to make sure that if the file contents passed to the ViewFile component change, the updated contents get displayed. Added a condition to the pre tag in the ViewFile component to print a newline character if the line is blank so that blank lines are preserved during copy-paste.

--- src/components/view_file.vue
+++ src/components/view_file.vue
@@ -7,9 +7,8 @@
         </div>
       </div>
       <div class="file-content-container">
-        <pre v-for="(line) of file_contents.split('\n')" class="line-of-file-content">
-          {{line}}
-        </pre>
+        <pre v-for="(line) of file_contents.split('\n')"
+             class="line-of-file-content">{{line === "" ? "\n" : line}}</pre>
       </div>
     </div>
   </div>
@@ -48,54 +47,59 @@
 </script>
 
 <style scoped lang="scss">
-  @import '@/styles/colors.scss';
+@import '@/styles/colors.scss';
 
-  #viewing-container {
-    border-radius: 0 0 3px 3px;
-    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-    padding: 10px 0 0 0;
-    width: 100%;
-  }
+#viewing-container {
+  border-radius: 0 0 3px 3px;
+  font-family: monospace;
+  padding: 5px 0 0 0;
+  width: 100%;
+}
 
-  #scrollable-container {
-    margin: 0;
-    overflow: scroll;
-    padding: 0;
-    position: relative;
-    width: 100%;
-  }
+#scrollable-container {
+  margin: 0;
+  overflow: scroll;
+  padding: 0;
+  position: relative;
+  width: 100%;
+}
 
-  .line-numbers-container {
-    position: absolute;
-    vertical-align: top;
-    width: 51px;
-  }
+.line-numbers-container {
+  position: absolute;
+  vertical-align: top;
+  user-select: none;
+  width: 51px;
+}
 
-  .line-number {
-    color: $baking-pan;
-    font-size: 13px;
-    padding: 1px 0 1px 0;
-    text-align: center;
-    z-index: 10;
-    height: 16px;
-  }
+.line-number {
+  color: $baking-pan;
+  font-size: 13px;
+  padding: 1px 0 1px 0;
+  text-align: center;
+  height: 16px;
+  border-bottom: 1px solid transparent;
+  vertical-align: top;
+}
 
-  .file-content-container {
-    color: black;
-    display: inline-block;
-    position: relative;
-    vertical-align: top;
-  }
+.file-content-container {
+  color: black;
+  display: inline-block;
+  margin-left: 51px;
+  position: relative;
+  vertical-align: top;
+}
 
-  .line-of-file-content {
-    font-size: 13px;
-    height: 16px;
-    margin: 0;
-    padding: 1px 10px 1px 0;
-  }
+.line-of-file-content {
+  font-size: 13px;
+  height: 16px;
+  margin: 0;
+  padding: 1px 10px 1px 0;
+  white-space: pre-wrap;
+  border-bottom: 1px solid transparent;
+}
 
-  .line-of-file-content:last-child {
-    margin-bottom: 5px;
-  }
+.line-of-file-content:last-child {
+  padding-bottom: 5px;
+}
 
 </style>
