4ded8c293804f13ec2352b866558db214348b77a
James Perretta <jameslp@umich.edu>
2018-10-09 18:25:53 -0400

    Tweaked exception string in SafeMap.

--- src/safe_map.ts
+++ src/safe_map.ts
@@ -2,52 +2,52 @@ export class SafeMapError extends Error {}
 
 export class SafeMap<K, V>  {
 
-    private _my_map = new Map<K, V>();
+    private _map = new Map<K, V>();
 
     get size() {
-        return this._my_map.size;
+        return this._map.size;
     }
 
     clear() {
-        this._my_map.clear();
+        this._map.clear();
     }
 
     delete(key: K) {
-        this._my_map.delete(key);
+        this._map.delete(key);
     }
 
     for_each(callbackfn: (value: V, index: K, map: Map<K, V>) => void): void {
-        return this._my_map.forEach(callbackfn);
+        return this._map.forEach(callbackfn);
     }
 
     get(key: K): V {
-        if (!this._my_map.has(key)) {
-            throw new SafeMapError("Key: " + key + ", is not in the map!");
+        if (!this._map.has(key)) {
+            throw new SafeMapError(`Key Error: "${key}" not found in map`);
         }
-        return this._my_map.get(key)!;
+        return this._map.get(key)!;
     }
 
     has(key: K): boolean {
-        return this._my_map.has(key);
+        return this._map.has(key);
     }
 
     set(key: K, value: V): Map<K, V> {
-        return this._my_map.set(key, value);
+        return this._map.set(key, value);
     }
 
     entries(): Iterator<[K, V]> {
-        return this._my_map.entries();
+        return this._map.entries();
     }
 
     keys(): Iterator<K> {
-        return this._my_map.keys();
+        return this._map.keys();
     }
 
     values(): Iterator<V> {
-        return this._my_map.values();
+        return this._map.values();
     }
 
     [Symbol.iterator]() {
-        return this._my_map[Symbol.iterator]();
+        return this._map[Symbol.iterator]();
     }
 }
