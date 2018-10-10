010d129cb0dd18f78f420b6c2a0eed7c5dc61855
Ashley Berg <ashberg@umich.edu>
2018-10-10 18:31:01 -0400

    Removed the Watch import from the typeahead because it was no longer being used.

--- src/components/dropdown_typeahead.vue
+++ src/components/dropdown_typeahead.vue
@@ -31,7 +31,7 @@
 <script lang="ts">
 import Dropdown from '@/components/dropdown.vue';
 
-import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component({
   components: { Dropdown }
 })
