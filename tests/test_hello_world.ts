cd6861f790b28743b91f1469a19bd0a7aa744e8a
James Perretta <jameslp@umich.edu>
2018-10-19 18:27:06 -0400

    Working on client imports.

--- tests/test_hello_world.ts
+++ tests/test_hello_world.ts
@@ -1,9 +1,12 @@
 import HelloWorld from '@/components/hello_world.vue';
 
+import { Course, Semester } from 'ag-client-typescript';
 import { mount } from '@vue/test-utils';
 
 describe('HelloWorld.vue', () => {
     test('renders props.msg when passed', () => {
+        let course = new Course('spam', Semester.fall, 2020, '', 0, '');
+        console.log(course);
         // const msg = 'new message';
         const wrapper = mount(HelloWorld);
 
