badb3d54cf0ba2aa81cd0e5d078fb74042cc55cb
Ashley Berg <ashberg@umich.edu>
2018-11-08 16:38:34 -0500

    Created the Model and ObserverComponent. Reorganized the functions in the course_list component to use less code when sorting the terms and courses and handle the cases when a term's semester and/or year are null. Created the array_get_unique function in utils.ts and added tests to test_utils.ts to check that array_get_unique behaves correctly for different inputs.

--- src/demos/course_list_demo.vue
+++ src/demos/course_list_demo.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script lang="ts">
+  import { HttpClient } from 'ag-client-typescript';
   import { Component, Prop, Vue } from 'vue-property-decorator';
 
   import CourseList from '@/components/course_list.vue';
@@ -12,7 +13,16 @@
   @Component({
     components: { CourseList}
   })
-  export default class CourseListDemo extends Vue {}
+  export default class CourseListDemo extends Vue {
+    created() {
+      HttpClient.set_base_url('http://localhost:9000/api/');
+      // HttpClient.set_default_headers({
+      //   // Note: Make sure the test server is using fake authentication.
+      //   Cookie: 'username=multi_role'
+      // });
+      // console.log('WAAAAAAAA');
+    }
+  }
 
 </script>
 
