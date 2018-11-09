c815d29e463b0e55053671cf375dd6c9b4e60083
James P <jameslp@umich.edu>
2018-11-08 21:29:22 -0500

    Added config files for running a docker dev stack.

--- src/demos/course_list_demo.vue
+++ src/demos/course_list_demo.vue
@@ -13,16 +13,7 @@
   @Component({
     components: { CourseList}
   })
-  export default class CourseListDemo extends Vue {
-    created() {
-      HttpClient.set_base_url('http://localhost:9000/api/');
-      // HttpClient.set_default_headers({
-      //   // Note: Make sure the test server is using fake authentication.
-      //   Cookie: 'username=multi_role'
-      // });
-      // console.log('WAAAAAAAA');
-    }
-  }
+  export default class CourseListDemo extends Vue {}
 
 </script>
 
