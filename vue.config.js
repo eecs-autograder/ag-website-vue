3504f158ce5fb9863eb93876a8c2ba15e5193208
James P <jameslp@umich.edu>
2018-11-08 21:29:22 -0500

    Added config files for running a docker dev stack.

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://django:8001'
+            },
+            '/static': {
+                target: 'http://django:8001'
+            },
+            '/__debug__/': {
+                target: 'http://django:8001'
+            },
+        }
+    }
+}
