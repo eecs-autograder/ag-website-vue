88d8221be30a6794db9931fc4241cdb55b37b905
James Perretta <jameslp@umich.edu>
2018-11-13 15:13:56 -0500

    Added function for patching async class methods.

--- tests/test_hello_world.ts
+++ tests/test_hello_world.ts
@@ -1,8 +1,17 @@
 import { Course, Semester } from 'ag-client-typescript';
 
-test('Can import and use classes from typescript client', () => {
+import { patch_async_class_method } from "./mocking";
+
+test('Can import and use classes from typescript client', async () => {
     let course = new Course(
         {pk: 42, name: 'spam', semester: Semester.fall, year: 2020, subtitle: '',
          num_late_days: 0, last_modified: ''});
     console.log(course);
+
+    await patch_async_class_method(
+            Course, 'save', () =>  Promise.resolve(42), async () => {
+       let mock_result = await course.save();
+       expect(mock_result).toEqual(42);
+       console.log(Course.prototype['save']);
+    });
 });
