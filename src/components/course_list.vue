55acf40ccca1b7d32157b92508ce160125bf9a54
Ashley Berg <ashberg@umich.edu>
2018-11-21 13:49:28 -0500

    Added a tslint comment to silence the linter about beforeDestroy.

--- src/components/course_list.vue
+++ src/components/course_list.vue
@@ -57,6 +57,7 @@
     all_courses: AllCourses | null = null;
     courses_by_term: TermCourses[] = [];
 
+    // tslint:disable-next-line:naming-convention
     beforeDestroy() {
       super.beforeDestroy();
     }
