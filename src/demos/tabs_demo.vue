711e8a1efd374294d2bba7861f37dc3372a428da
Ashley Berg <ashberg@umich.edu>
2018-12-07 17:57:09 -0500

    Added a tab theme for tabs that contain a tab header that contains a dropdown menu.

--- src/demos/tabs_demo.vue
+++ src/demos/tabs_demo.vue
@@ -6,16 +6,16 @@
 
       <br> <br>
       <div class="shrink-tabs">
-        <p> Current Tab Index = {{current_tab_index}}</p>
+        <p> Current Tab Index = {{current_tab_index_1}}</p>
         <p class="tab-styling-theme-label"> GRAY THEME </p>
-        <div class="limit-tabs">
-        <tabs ref="tabs" v-model="current_tab_index"
+        <p> These tab-headings have overflow: hidden and a set height. </p>
+        <tabs ref="tabs-gray" v-model="current_tab_index_1"
               tab_active_class="gray-theme-active"
               tab_inactive_class="gray-theme-inactive">
           <tab v-for="(tab_val, index) in tab_labels" :key="tab_val">
             <tab-header v-on:click="log_event">
-              <span class="tab-heading"> Tab {{tab_val}} </span>
-              <i class="fas fa-times close_x"
+              <div class="tab-heading"> Tab {{tab_val}} is a long tab</div>
+              <i class="fas fa-times close-x"
                   @click="$event.stopPropagation(); remove_tab(index)"></i>
             </tab-header>
             <template slot="body">
@@ -25,99 +25,143 @@
             </template>
           </tab>
         </tabs>
-        </div>
+
+        <p class="tab-styling-theme-label"> Tab with a Dropdown </p>
+        <tabs ref="tabs-gray-no-padding" v-model="current_tab_index_2"
+              tab_active_class="gray-theme-active-no-padding"
+              tab_inactive_class="gray-theme-inactive-no-padding">
+          <tab>
+            <tab-header>
+
+              <dropdown ref="countries_dropdown"
+                        :items="countries"
+                        @update_item_selected="country_selected = $event">
+                <template slot="header">
+                  <div class="tab-label" tabindex="1">
+                    <p class="tab-heading-dropdown-tabs"
+                       ref="edit_roster_tab"
+                       @click="show_countries_dropdown">
+                      {{country_selected}}
+                    </p>
+                  </div>
+                </template>
+                <div slot-scope="{item}">
+                  <div class="edit-rosters-dropdown-row-content">
+                    <span>{{item}}</span>
+                  </div>
+                </div>
+              </dropdown>
+
+            </tab-header>
+
+            <template slot="body">
+              <div class="tab-body-gray-no-padding">
+              </div>
+            </template>
+          </tab>
+          <tab>
+            <tab-header>
+              <div class="tab-heading-dropdown-tabs"> Another tab </div>
+            </tab-header>
+            <template slot="body">
+              <div class="tab-body-gray-no-padding">
+                Happy Holidays
+              </div>
+            </template>
+          </tab>
+        </tabs>
 
         <br>
-        <!--<p class="tab-styling-theme-label"> DARK THEME </p>-->
-        <!--<tabs ref="tabs" v-model="current_tab_index"-->
-              <!--tab_active_class="dark-theme-active"-->
-              <!--tab_inactive_class="dark-theme-inactive">-->
-          <!--<tab v-for="(tab_val, index) in tab_labels" :key="tab_val">-->
-            <!--<tab-header v-on:click="log_event">-->
-              <!--<span class="tab-heading"> Tab {{tab_val}} </span>-->
-              <!--<i class="fas fa-times close_x"-->
-                 <!--@click="$event.stopPropagation(); remove_tab(index)"></i>-->
-            <!--</tab-header>-->
-            <!--<template slot="body">-->
-              <!--<div class="tab-body-dark">-->
-                <!--{{tab_val}}-->
-              <!--</div>-->
-            <!--</template>-->
-          <!--</tab>-->
-        <!--</tabs>-->
+        <p class="tab-styling-theme-label"> GRAY-WHITE THEME </p>
+        <tabs ref="tabs-gray-white" v-model="current_tab_index_3"
+              tab_active_class="gray-white-theme-active"
+              tab_inactive_class="gray-white-theme-inactive">
+          <tab v-for="(tab_val, index) in tab_labels" :key="tab_val">
+            <tab-header v-on:click="log_event">
+              <div class="tab-heading"> Tab {{tab_val}} </div>
+              <i class="fas fa-times close-x"
+                 @click="$event.stopPropagation(); remove_tab(index)"></i>
+            </tab-header>
+            <template slot="body">
+              <div class="tab-body-gray-white">
+                {{tab_val}}
+              </div>
+            </template>
+          </tab>
+        </tabs>
 
         <br>
-        <!--<p class="tab-styling-theme-label"> BLUE THEME </p>-->
-        <!--<tabs ref="tabs" v-model="current_tab_index"-->
-              <!--tab_active_class="blue-theme-active"-->
-              <!--tab_inactive_class="blue-theme-inactive">-->
-          <!--<tab v-for="(tab_val, index) in tab_labels" :key="tab_val">-->
-            <!--<tab-header v-on:click="log_event">-->
-              <!--<span class="tab-heading"> Tab {{tab_val}} </span>-->
-              <!--<i class="fas fa-times close_x"-->
-                 <!--@click="$event.stopPropagation(); remove_tab(index)"></i>-->
-            <!--</tab-header>-->
-            <!--<template slot="body">-->
-              <!--<div class="tab-body-blue">-->
-                <!--{{tab_val}}-->
-              <!--</div>-->
-            <!--</template>-->
-          <!--</tab>-->
-        <!--</tabs>-->
+        <p class="tab-styling-theme-label"> NO BORDER THEME </p>
+        <tabs ref="tabs-no-border" v-model="current_tab_index_4"
+              tab_active_class="no-border-active"
+              tab_inactive_class="no-border-inactive">
+          <tab v-for="(tab_val, index) in tab_labels" :key="tab_val">
+            <tab-header v-on:click="log_event">
+              <div class="tab-heading"> Tab {{tab_val}} </div>
+              <i class="fas fa-times close-x"
+                 @click="$event.stopPropagation(); remove_tab(index)"></i>
+            </tab-header>
+            <template slot="body">
+              <div class="tab-body-no-border">
+                {{tab_val}}
+              </div>
+            </template>
+          </tab>
+        </tabs>
 
         <br>
-        <!--<p class="tab-styling-theme-label"> NO BORDER THEME </p>-->
-        <!--<tabs ref="tabs" v-model="current_tab_index"-->
-              <!--tab_active_class="no-border-active"-->
-              <!--tab_inactive_class="no-border-inactive">-->
-          <!--<tab v-for="(tab_val, index) in tab_labels" :key="tab_val">-->
-            <!--<tab-header v-on:click="log_event">-->
-              <!--<span class="tab-heading"> Tab {{tab_val}} </span>-->
-              <!--<i class="fas fa-times close_x"-->
-                 <!--@click="$event.stopPropagation(); remove_tab(index)"></i>-->
-            <!--</tab-header>-->
-            <!--<template slot="body">-->
-              <!--<div class="tab-body-no-border">-->
-                <!--{{tab_val}}-->
-              <!--</div>-->
-            <!--</template>-->
-          <!--</tab>-->
-        <!--</tabs>-->
+        <p class="tab-styling-theme-label"> DARK THEME </p>
+        <tabs ref="tabs-dark" v-model="current_tab_index_5"
+              tab_active_class="dark-theme-active"
+              tab_inactive_class="dark-theme-inactive">
+          <tab v-for="(tab_val, index) in tab_labels" :key="tab_val">
+            <tab-header v-on:click="log_event">
+              <div class="tab-heading"> Tab {{tab_val}} </div>
+              <i class="fas fa-times close-x"
+                 @click="$event.stopPropagation(); remove_tab(index)"></i>
+            </tab-header>
+            <template slot="body">
+              <div class="tab-body-dark">
+                {{tab_val}}
+              </div>
+            </template>
+          </tab>
+        </tabs>
 
         <br>
-        <!-- This one uses the default styling -->
-        <!--<p class="tab-styling-theme-label"> WHITE THEME </p>-->
-        <!--<tabs ref="tabs2">-->
-          <!--<tab>-->
-            <!--<tab-header v-on:click="log_event">-->
-              <!--<p class="tab-heading"> Cat Tab </p>-->
-            <!--</tab-header>-->
-            <!--<template slot="body">-->
-              <!--<div class="tab-body-white">-->
-              <!--</div>-->
-            <!--</template>-->
-          <!--</tab>-->
-          <!--<tab>-->
-            <!--<tab-header v-on:click.native="log_event">-->
-              <!--<p class="tab-heading"> Dog Tab </p>-->
-            <!--</tab-header>-->
-            <!--<template slot="body">-->
-              <!--<div class="tab-body-white">-->
-                <!--Bye 2-->
-              <!--</div>-->
-            <!--</template>-->
-          <!--</tab>-->
-          <!--<tab>-->
-            <!--<tab-header @click.native="log_event">-->
-              <!--Bare Text Tab-->
-            <!--</tab-header>-->
-            <!--<template slot="body">-->
-              <!--<div class="tab-body-white">-->
-                <!--The tab header has just text-->
-              <!--</div>-->
-            <!--</template>-->
-          <!--</tab>-->
-        <!--</tabs>-->
+        <p> This one uses the default styling </p>
+        <p class="tab-styling-theme-label"> WHITE THEME </p>
+        <tabs ref="tabs-white">
+          <tab>
+            <tab-header v-on:click="log_event">
+              <span class="div-not-bare"> Tab 1 </span>
+            </tab-header>
+            <template slot="body">
+              <div class="tab-body-white">
+              </div>
+            </template>
+          </tab>
+          <tab>
+            <tab-header v-on:click.native="log_event">
+              <span class="div-not-bare"> Tab 2 </span>
+            </tab-header>
+            <template slot="body">
+              <div class="tab-body-white">
+                Bye 2
+              </div>
+            </template>
+          </tab>
+          <tab>
+            <tab-header @click.native="log_event">
+              Bare Text Tab 3
+            </tab-header>
+            <template slot="body">
+              <div class="tab-body-white">
+                The tab header has just text
+              </div>
+            </template>
+          </tab>
+        </tabs>
       </div>
     </div>
   </div>
@@ -127,20 +171,35 @@
 
   import { Component, Vue } from 'vue-property-decorator';
 
+  import Dropdown from '@/components/dropdown.vue';
   import Tab from '@/components/tabs/tab.vue';
   import TabHeader from '@/components/tabs/tab_header.vue';
   import Tabs from '@/components/tabs/tabs.vue';
   import ViewFile from '@/components/view_file.vue';
 
   @Component({
-    components: {Tabs, TabHeader, Tab, ViewFile}
+    components: {Dropdown, Tabs, TabHeader, Tab, ViewFile}
   })
   export default class TabsDemo extends Vue {
+    countries = ["France", "Norway", "Spain"];
+
+    country_selected = "France";
+
+    show_countries_dropdown() {
+      let countries_dropdown = <Dropdown> this.$refs.countries_dropdown;
+      countries_dropdown.show_the_dropdown_menu();
+      event.stopPropagation();
+    }
+
     log_event(e: Event) {
       console.log(e);
     }
 
-    current_tab_index = 1;
+    current_tab_index_1 = 1;
+    current_tab_index_2 = 1;
+    current_tab_index_3 = 1;
+    current_tab_index_4  = 1;
+    current_tab_index_5 = 1;
 
     tab_labels = [1, 2, 3];
 
@@ -162,15 +221,22 @@
 <style scoped lang="scss">
   @import '@/styles/colors.scss';
 
-  .close_x:hover {
+  .tab-label {
+    outline: none;
+  }
+
+  .close-x:hover {
     color: $warning-red;
     cursor: pointer;
   }
 
-  .close_x {
+  .close-x {
     position: absolute;
-    right: 20px;
-    top: 12px;
+    right: 10px;
+    top: 7.5px;
+    font-size: 20px;
+    background-color: inherit;
+    padding: 10px 8px;
   }
 
   .tab-body {
@@ -181,19 +247,19 @@
     position: relative;
   }
 
-  .tab-body-gray {
+  .tab-body-gray, .tab-body-gray-no-padding {
     @extend .tab-body;
     border: 2px solid $stormy-gray-dark;
+  }
+
+  .tab-body-gray-white {
+    @extend .tab-body;
+    border: 2px solid $pebble-dim;
   }
 
   .tab-body-dark{
     @extend .tab-body;
     border: 2px solid darken($stormy-gray-dark, 25);
-  }
-
-  .tab-body-blue {
-    @extend .tab-body;
-    border: 2px solid darken($ocean-blue, 15);
   }
 
   .tab-body-no-border {
@@ -202,13 +268,21 @@
 
   .tab-body-white {
     @extend .tab-body;
-    border: 2px solid $pebble-dark;
+    border: 2px solid $pebble-dim;
   }
 
   .tab-heading {
     margin: 0;
-    /*padding-right: 15px;*/
     font-family: "Helvetica Neue", Helvetica;
+    height: 19px;
+    overflow: hidden;
+  }
+
+  .tab-heading-dropdown-tabs {
+    margin: 0;
+    height: 19px;
+    padding: 18px 20px;
+    overflow: hidden;
   }
 
   .shrink-tabs {
@@ -223,10 +297,17 @@
     display: inline-block;
     border: 2px dashed $pebble-dark;
   }
-  /*}*/
 
-  /*.limit-tabs {*/
-    /*width: 300px;*/
-  /*}*/
+  @media only screen and (min-width: 481px) {
+      .close-x {
+        padding: 5px;
+        top: 6.5px;
+        font-size: inherit;
+      }
+
+      .tab-heading-dropdown-tabs {
+        padding: 10px 15px;
+      }
+  }
 
 </style>
