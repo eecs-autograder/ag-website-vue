8d748bfab224142a4e08338b6d68b4362d799ae7
ffuxa <ffuxa@umich.edu>
2018-12-19 21:25:59 -0500

    Fixed provide/inject to create working prototype

--- src/demos/validated_form_demo.vue
+++ src/demos/validated_form_demo.vue
@@ -1,19 +1,39 @@
 <template>
-  <validated-form @submit="save_data" ref="form1">
-    <validated-input v-model="data.id"
-                     :validators="[is_number]"
-                     :from_string_fn="(val) => parseInt(val, 10)"></validated-input>
-    <div id="hello">
-      <p>here</p>
-      <validated-input v-model="data.name" :validators="[(val) => [val === 'mars', 'not mars']]"></validated-input>
-    </div>
+  <div style="padding: 10px;">
+    <hr/>
+    <h1>Validated Form</h1>
+    <validated-form @submit="save_data" ref="form1">
+      <h3>Validated Input 1</h3>
+      <p>Has to be a number</p>
+      <validated-input v-model="data.id"
+                       :validators="[is_number]"
+                       :from_string_fn="(val) => parseInt(val, 10)"></validated-input>
 
-    <button type="submit" :disabled="loading || !$refs.form1.is_valid()">Save</button>
-  </validated-form>
-  <!--<div>-->
-    <!--<validated-form ref="form1">hello</validated-form>-->
-    <!--<h3>{{loading || this.$refs.form1.is_valid()}}</h3>-->
-  <!--</div>-->
+      <div id="hello">
+        <h3>Validated Input 2</h3>
+        <p>Has to be the string "mars"</p>
+        <validated-input v-model="data.name" :validators="[(val) => {
+                                                            return {
+                                                              is_valid: val === 'mars',
+                                                              error_msg: 'not mars'
+                                                            }
+                                                           }]"></validated-input>
+      </div>
+
+      <p>
+        The save button below is disabled (red) if any of the above inputs are invalid.
+        Otherwise, it is enabled (blue).
+      </p>
+      <button type="submit"
+              :disabled="loading || !$refs.form1.is_valid"
+              :class="{
+                disabled: loading || !$refs.form1.is_valid,
+                enabled: loading || $refs.form1.is_valid
+              }">Save</button>
+      <h3>Validated Form is_valid status: {{loading || this.$refs.form1.is_valid}}</h3>
+      <br/><br/>
+    </validated-form>
+  </div>
 </template>
 
 <script lang="ts">
@@ -59,4 +79,11 @@
 </script>
 
 <style scoped lang="scss">
+  .disabled {
+    background-color: red;
+  }
+
+  .enabled {
+    background-color: lightblue;
+  }
 </style>
