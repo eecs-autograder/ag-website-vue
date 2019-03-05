15bac5fbc3a47b2a54e49a80eab9d04b3973399b
Ashley Berg <ashberg@umich.edu>
2019-03-05 08:36:00 -0500

    Beginning to transfer the contents of the course_settings tab from the original 64-branch.

---
+++ src/components/course_settings.vue
@@ -0,0 +1,335 @@
+<template>
+  <div id="settings-container">
+    <div id="settings-container-inputs">
+      <ValidatedForm id="course-settings-form"
+                     autocomplete="off"
+                     @submit.native.prevent="save_course_settings"
+                     @form_validity_changed="settings_form_is_valid = $event">
+
+        <div class="name-container">
+          <label class="settings-input-label"> Course name: </label>
+          <ValidatedInput ref="course_name"
+                          v-model="course.name"
+                          input_style="width: 100%; max-width: 500px; border-width: 2px"
+                          :validators="[is_not_empty]"
+                          :num_rows="1">
+          </ValidatedInput>
+        </div>
+
+        <div class="semester-container">
+          <label class="settings-input-label"> Semester: </label>
+          <div class="semester-dropdown-wrapper">
+            <dropdown ref="semester_dropdown"
+                      :items="semesters"
+                      @update_item_selected="course.semester = $event">
+              <template slot="header">
+                <div tabindex="1" class="input-wrapper">
+                  <div id="input-course-semester"
+                       class="settings-input">
+                    {{course.semester}}
+                    <i class="fas fa-caret-down dropdown-caret"></i>
+                  </div>
+                </div>
+              </template>
+              <div slot-scope="{item}">
+                <span class="semester-item">{{item}}</span>
+              </div>
+            </dropdown>
+          </div>
+        </div>
+
+        <div class="year-container">
+          <label class="settings-input-label"> Year: </label>
+          <ValidatedInput ref="course_year"
+                          v-model="course.year"
+                          :num_rows="1"
+                          input_style="width: 65px; border-width: 2px"
+                          :validators="[is_not_empty, is_number, is_valid_year]">
+          </ValidatedInput>
+        </div>
+
+
+        <div class="late-days-container">
+          <label class="settings-input-label"> Late days per student: </label>
+          <ValidatedInput ref="course_late_days"
+                          v-model="course.num_late_days"
+                          :num_rows="1"
+                          input_style="width: 50px; border-width: 2px"
+                          :validators="[is_not_empty, is_number, is_non_negative]">
+            <div slot="suffix" class="suffix-element">
+              {{ course.num_late_days === 1 ? 'day' : 'days'}} </div>
+          </ValidatedInput>
+        </div>
+
+        <div v-for="error of api_errors"
+             class="api-error-container">
+          <div class="api-error"> {{error}} </div>
+          <div class="x-box">
+            <span @click="api_errors = []" class="dismiss-error"> Dismiss </span>
+          </div>
+        </div>
+
+        <input id="settings-submit"
+               type="submit"
+               class="submit-button"
+               value="Save Updates"
+               :disabled="!settings_form_is_valid || saving">
+        <div v-if="!saving"
+             class="last-saved-timestamp">
+          <span> Last Saved: </span>
+          {{(new Date(course.last_modified)).toLocaleString(
+          'en-US', last_modified_format
+          )}}
+        </div>
+        <div v-else class="last-saved-spinner">
+          <i class="fa fa-spinner fa-pulse"></i>
+        </div>
+
+      </ValidatedForm>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Course, Semester } from 'ag-client-typescript';
+  import { AxiosResponse } from 'axios';
+  import Dropdown from '@/components/dropdown.vue';
+  import Tooltip from '@/components/tooltip.vue';
+  import ValidatedForm from '@/components/validated_form.vue';
+  import ValidatedInput, { ValidatorResponse } from '@/components/validated_input.vue';
+  import { handle_400_errors_async } from '@/utils';
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component({
+    components: {
+      Dropdown,
+      Tooltip,
+      ValidatedForm,
+      ValidatedInput
+    }
+  })
+  export default class CourseSettings extends Vue {
+    @Prop({required: true, type: Course})
+    course!: Course;
+
+    api_errors: string[] = [];
+    d_course!: Course;
+    last_modified_format = {year: 'numeric', month: 'long', day: 'numeric',
+                            hour: 'numeric', minute: 'numeric', second: 'numeric'};
+    saving = false;
+    semesters = [Semester.fall, Semester.winter, Semester.spring, Semester.summer];
+    settings_form_is_valid = false;
+
+    is_number(value: string): ValidatorResponse {
+      return {
+        is_valid: value !== "" && !isNaN(Number(value)),
+        error_msg:  "You must enter a number.",
+      };
+    }
+
+    is_not_empty(value: string): ValidatorResponse {
+      return {
+        is_valid: value !== "",
+        error_msg: "This field is required."
+      };
+    }
+
+    is_non_negative(value: string): ValidatorResponse {
+      return {
+        is_valid: this.is_number(value).is_valid && value[0] !== "-",
+        error_msg: "The number of late days cannot be negative."
+      };
+    }
+
+    created() {
+      this.d_course = this.course;
+    }
+
+    is_valid_year(value: string): ValidatorResponse {
+      return {
+        is_valid: Number(value) >= 2000,
+        error_msg: "Please enter a valid year."
+      };
+    }
+
+    @handle_400_errors_async(handle_save_course_settings_error)
+    async save_course_settings() {
+      try {
+        this.saving = true;
+        this.api_errors = [];
+        let response = await this.course.save();
+      }
+      finally {
+        this.saving = false;
+      }
+    }
+  }
+
+  function handle_save_course_settings_error(component: CourseSettings, response: AxiosResponse) {
+    let errors = response.data["__all__"];
+    if (errors !== undefined && errors.length > 0) {
+      component.api_errors = [errors[0]];
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/colors.scss';
+@import '@/styles/button_styles.scss';
+@import url('https://fonts.googleapis.com/css?family=Quicksand');
+$current-lang-choice: "Quicksand";
+$github-black-color: #24292e;
+
+#settings-container {
+  font-family: $current-lang-choice;
+  margin: 5px 0;
+}
+
+.x-box {
+  position: absolute;
+  right: 6px;
+  top: 6px;
+  padding: 4px 10px;
+  background-color: white;
+  border-radius: .25rem;
+  cursor: pointer;
+  border: 1px solid #f5c6cb;
+}
+
+.api-error-container {
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 500px;
+  position: relative;
+  color: #721c24;
+  background-color: #f8d7da;
+  border: 1px solid #f5c6cb;
+  padding: 10px 70px 10px 10px;
+  border-radius: .25rem;
+  margin-bottom: 11px;
+}
+
+.submit-button {
+  @extend .green-button;
+  display: block;
+  font-family: $current-lang-choice;
+  font-size: 16px;
+  padding: 10px 15px;
+  margin: 10px 0;
+}
+
+.submit-button:disabled {
+  @extend .gray-button;
+}
+
+.submit-button:disabled:hover {
+  background-color: hsl(210, 13%, 63%);
+  cursor: default;
+}
+
+#settings-container-inputs {
+  margin: 0 20px;
+}
+
+.settings-input-label {
+  text-align: left;
+  font-size: 17px;
+  font-weight: 600;
+  margin: 5px 15px 7px 0;
+  display: inline-block;
+  color: $github-black-color;
+}
+
+.settings-input {
+  position: relative;
+  display: block;
+  width: 100%;
+  padding: .375rem .75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: $github-black-color;
+  background-color: #fff;
+  border: 2px solid #ced4da;
+  border-radius: .25rem;
+  transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+}
+
+.settings-input:focus {
+  border-color: $ocean-blue;
+}
+
+.semester-dropdown-wrapper {
+  display: block;
+}
+
+.name-container, .year-container, .semester-container, .late-days-container {
+  padding-bottom: 16px;
+  display: block;
+  max-width: 500px;
+}
+
+.late-days-container {
+  margin-bottom: 50px;
+}
+
+#input-course-semester {
+  width: 120px;
+  height: 24px;
+}
+
+.semester-item {
+  font-size: 17px;
+}
+
+.last-saved-timestamp {
+  padding-top: 6px;
+  margin: 0;
+  font-size: 15px;
+  color: lighten(#495057, 40);
+}
+
+.last-saved-spinner {
+  font-size: 18px;
+  color: $github-black-color;
+  display: inline-block;
+}
+
+.input-wrapper {
+  position: relative;
+  display: inline-block;
+  margin: 0;
+}
+
+.dropdown-caret {
+  position: absolute;
+  right: 18px;
+  top: 4px;
+  font-size: 30px;
+  cursor: pointer;
+}
+
+.suffix-element {
+  display: inline-block;
+  vertical-align: top;
+  padding-top: 10px;
+  padding-left: 10px;
+}
+
+@media only screen and (min-width: 481px) {
+  .submit-button, .submit-button:disabled {
+    padding: 10px 15px;
+    font-family: $current-lang-choice;
+    font-size: 16px;
+    margin: 0 15px 12px 0;
+    display: inline-block;
+  }
+
+  #settings-container {
+    margin: 0;
+  }
+
+  #settings-container-inputs {
+    margin: 0 0 0 40px;
+  }
+}
+</style>
