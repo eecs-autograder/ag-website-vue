5edc488891a7a6ac3f9a8a7b8f4cbed584be63c2
Ashley Berg <ashberg@umich.edu>
2019-04-04 10:19:47 -0400

    Added the skeleton of the ProjectSubmission component.

---
+++ src/components/project_submission/project_submission.vue
@@ -0,0 +1,149 @@
+<template>
+  <div v-if="d_loading" class="loading-spinner">
+    <i class="fa fa-spinner fa-pulse"></i>
+  </div>
+
+  <div v-else
+       id="project-submission">
+    <tabs ref="course_admin_tabs"
+          tab_active_class="gray-white-theme-active"
+          tab_inactive_class="gray-white-theme-inactive"
+          v-model="current_tab_index"
+          @input="on_tab_changed">
+      <tab>
+        <tab-header ref="project_settings_tab">
+          <div class="tab-label">
+            <p class="tab-header"> Submit </p>
+          </div>
+        </tab-header>
+        <template slot="body">
+          <div class="tab-body">
+            Submit - TODO
+          </div>
+        </template>
+      </tab>
+
+      <tab>
+        <tab-header ref="instructor_files_tab">
+          <div class="tab-label">
+            <p class="tab-header"> My Submissions </p>
+          </div>
+        </tab-header>
+        <template slot="body">
+          <div class="tab-body">
+            Submissions - TODO
+          </div>
+        </template>
+      </tab>
+
+      <tab>
+        <tab-header ref="expected_student_files_tab">
+          <div class="tab-label">
+            <p class="tab-header"> Student Lookup </p>
+          </div>
+        </tab-header>
+        <template slot="body">
+          <div class="tab-body">
+            Student Lookup - TODO (branch Exists)
+          </div>
+        </template>
+      </tab>
+    </tabs>
+  </div>
+</template>
+
+
+<script lang="ts">
+  import Tab from '@/components/tabs/tab.vue';
+  import TabHeader from '@/components/tabs/tab_header.vue';
+  import Tabs from '@/components/tabs/tabs.vue';
+  import { get_query_param } from '@/utils';
+  import { Project } from 'ag-client-typescript';
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({
+    components: {
+      Tab,
+      TabHeader,
+      Tabs
+    }
+  })
+  export default class ProjectSubmission extends Vue {
+    current_tab_index = 0;
+    d_loading = true;
+    project: Project | null = null;
+
+    async created() {
+      this.project = await Project.get_by_pk(Number(this.$route.params.project_id));
+      this.d_loading = false;
+    }
+
+    mounted() {
+      this.select_tab(get_query_param(this.$route.query, "current_tab"));
+    }
+
+    on_tab_changed(index: number) {
+      switch (index) {
+        case 0:
+          this.$router.replace({query: {current_tab: "submit"}});
+          break;
+        case 1:
+          this.$router.replace({query: {current_tab: "my_submissions"}});
+          break;
+        case 2:
+          this.$router.replace({query: {current_tab: "student_lookup"}});
+      }
+    }
+    select_tab(tab_name: string | null) {
+      switch (tab_name) {
+        case "submit":
+          break;
+        case "my_submissions":
+          this.current_tab_index = 1;
+          break;
+        case "student_lookup":
+          this.current_tab_index = 2;
+          break;
+        default:
+          this.current_tab_index = 0;
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/colors.scss';
+
+.loading-spinner {
+  color: $ocean-blue;
+  font-size: 55px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.tab-header {
+  margin: 0;
+  font-size: 14px;
+  overflow: hidden;
+}
+
+.tab-body {
+  text-align: left;
+  position: relative;
+  padding-top: 10px;
+}
+
+.tab-label {
+  outline: none;
+  cursor: pointer;
+}
+
+@media only screen and (min-width: 481px) {
+  .tab-body {
+    border-top: 2px solid $pebble-medium;
+  }
+}
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import CourseAdmin from '@/components/course_admin/course_admin.vue';
 import CourseList from '@/components/course_list/course_list.vue';
 import ProjectAdmin from '@/components/project_admin/project_admin.vue';
+import ProjectSubmission from '@/components/project_submission/project_submission.vue';
 import UIDemos from '@/demos/ui_demos.vue';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -16,7 +17,8 @@ const ROUTES = [
     { path: '/', name: "course_list", component: CourseList },
     { path: '/web/course_admin/:course_id', name: "course_admin", component: CourseAdmin },
     { path: '/web/project_admin/:project_id', name: 'project_admin', component: ProjectAdmin },
-    { path: '/__demos__', component: UIDemos },
+    { path: '/web/project/:project_id', component: ProjectSubmission },
+    { path: '/__demos__', component: UIDemos }
 ];
 
 const ROUTER = new VueRouter ({
