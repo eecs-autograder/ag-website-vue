8f27d2fb01497c1dd5a3f8e649b957700e33d861
James Perretta <jameslp@umich.edu>
2019-04-08 20:07:05 -0400

    Updated line length checker to be able to ignore imports.

--- check_line_length.py
+++ check_line_length.py
@@ -18,6 +18,9 @@ def main():
 
             with open(filename) as f:
                 for line_num, line in enumerate(f, start=1):
+                    if ignore_line(line, args.ignore_startswith):
+                        continue
+
                     if len(line) > args.max_line_length:
                         too_long_lines.append((filename, line_num, len(line)))
 
@@ -29,11 +32,21 @@ def main():
         exit(1)
 
 
+def ignore_line(line, ignore_strings):
+    for ignore_string in ignore_strings:
+        if line.startswith(ignore_string):
+            return True
+
+    return False
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('max_line_length', type=int)
     parser.add_argument('file_patterns', nargs='+')
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-i', '--ignore_startswith', action='append', 
+                        help='Ignore lines that start with this string')
 
     return parser.parse_args()
 
