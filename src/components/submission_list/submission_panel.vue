6611d7a385c6e21d7cd69d1840e9378058da59f6
James Perretta <jameslp@umich.edu>
2019-09-05 14:32:03 -0400

    Cleanup

--- src/components/submission_list/submission_panel.vue
+++ src/components/submission_list/submission_panel.vue
@@ -4,25 +4,17 @@
       {{format_datetime_short(submission.timestamp)}}
     </div>
     <div class="submission-status">
-      <!-- <template v-if="submission.status === GradingStatus.received"> -->
-        <!-- <i class="far fa-paper-plane"></i> -->
-        <!-- <i class="fas fa-plane-arrival"></i> -->
-      <!-- </template> -->
       <template v-if="submission.status === GradingStatus.queued">
         <div class="queued-symbol">Q</div>
-        <!-- <i class="fas fa-list"></i> -->
       </template>
       <template v-else-if="submission.status === GradingStatus.being_graded">
         <i class="fas fa-list"></i>
-        <!-- <i class="fas fa-tasks"></i> -->
-        <!-- <i class="fas fa-sync-alt"></i> -->
       </template>
 
       <template v-else-if="submission.status === GradingStatus.waiting_for_deferred">
         <div v-if="submission.results.total_points_possible !== 0" class="score">
           {{submission.results.total_points}}/{{submission.results.total_points_possible}}
         </div>
-        <!-- <i v-else class="fas fa-check"></i> -->
         <i v-else class="far fa-check-circle"></i>
       </template>
       <template v-else-if="submission.status === GradingStatus.finished_grading">
@@ -30,14 +22,10 @@
           {{submission.results.total_points}}/{{submission.results.total_points_possible}}
         </div>
         <i v-else class="far fa-check-circle"></i>
-        <!-- <i v-else class="fas fa-check"></i> -->
       </template>
 
       <template v-else-if="submission.status === GradingStatus.removed_from_queue">
         <i class="fas fa-eject"></i>
-        <!-- <i class="fas fa-minus-circle"></i> -->
-        <!-- <i class="fas fa-hand-paper"></i> -->
-        <!-- <i class="far fa-hand-paper"></i> -->
       </template>
       <template v-else-if="submission.status === GradingStatus.error">
         <i class="fas fa-skull"></i>
@@ -90,15 +78,15 @@ export default class SubmissionPanel extends Vue {
       color: darken($sky-blue, 10%);
     }
 
-    .fa-tasks, .fa-list {
+    .fa-list {
       color: $ocean-blue;
     }
 
-    .fa-check, .fa-check-circle {
+    .fa-check-circle {
       color: green;
     }
 
-    .fa-eject, .fa-hand-paper, .fa-minus-circle {
+    .fa-eject {
       color: $orange;
     }
 
