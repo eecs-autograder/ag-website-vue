7aa4bce541ffd55a5d12b0aaea57375a4d83015c
James Perretta <jameslp@umich.edu>
2019-09-11 11:49:28 -0400

    Suppressed linter error in test.

--- tests/test_sticky_sidebar.ts
+++ tests/test_sticky_sidebar.ts
@@ -15,10 +15,18 @@ test('Sidebar height calculation', () => {
     expect(wrapper.vm.d_height).toEqual('calc(100vh - 0px)');
 
     // Workaround: vue-test-utils doesn't seem to support getBoundingClientRect
-    wrapper.vm.$refs.sidebar.getBoundingClientRect = () => {
-        return {top: 10};
+    (<HTMLElement> wrapper.vm.$refs.sidebar).getBoundingClientRect = () => {
+        return {
+            top: 10,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: 0,
+            width: 0,
+        };
     };
 
-    wrapper.vm.scroll_handler();
+    // tslint:disable-next-line: no-any
+    (<any> wrapper.vm).scroll_handler();
     expect(wrapper.vm.d_height).toEqual('calc(100vh - 10px)');
 });
