ac6a0f054cc5cb6dacc5cd6933cd70e8638efd55
James Perretta <jameslp@umich.edu>
2019-11-22 15:35:25 -0500

    Adjusted style and handling of new projects in manage_projects.vue.

--- src/components/course_admin/manage_projects/manage_projects.vue
+++ src/components/course_admin/manage_projects/manage_projects.vue
@@ -1,76 +1,64 @@
 <template>
   <div id="manage-projects-component">
-    <div>
-      <div id="new-project-side">
-        <div id="new-project-space">
+    <div id="new-project-label"> Create a New Project </div>
 
-          <p id="new-project-label"> Create a New Project </p>
+    <div class="new-project-validation-wrapper">
+      <ValidatedForm ref="new_project_form"
+                      autocomplete="off"
+                      spellcheck="false"
+                      @submit="add_project">
+        <ValidatedInput ref="new_project_name"
+                        v-model="new_project_name"
+                        :validators="[is_not_empty]"
+                        :num_rows="1"
+                        input_style="width: 100%;
+                                      max-width: 500px;
+                                      border: 1px solid #ced4da;"
+                        @input_validity_changed="new_project_name_is_valid = $event"
+                        :show_warnings_on_blur="true">
+        </ValidatedInput>
 
-          <div class="new-project-validation-wrapper">
-            <ValidatedForm ref="new_project_form"
-                           autocomplete="off"
-                           spellcheck="false"
-                           @submit="add_project">
-              <ValidatedInput ref="new_project_name"
-                              v-model="new_project_name"
-                              :validators="[is_not_empty]"
-                              :num_rows="1"
-                              input_style="width: 100%;
-                                           max-width: 500px;
-                                           border: 1px solid #ced4da;"
-                              @input_validity_changed="new_project_name_is_valid = $event"
-                              :show_warnings_on_blur="true">
-              </ValidatedInput>
+        <APIErrors ref="api_errors"></APIErrors>
 
-              <APIErrors ref="api_errors"></APIErrors>
-
-              <button type="submit"
-                      :disabled="!new_project_name_is_valid || d_adding_project"
-                      class="add-project-button">
-                Add Project
-              </button>
-
-            </ValidatedForm>
-          </div>
+        <div class="button-footer">
+          <button type="submit"
+                  :disabled="!new_project_name_is_valid || d_adding_project"
+                  class="save-button add-project-button">
+            Add Project
+          </button>
         </div>
-      </div>
-
-      <div id="existing-projects-side">
-        <p id="existing-projects-label"> Existing Projects </p>
-        <div v-if="loading"
-             class="loading-projects">
-          <i class="fa fa-spinner fa-pulse"></i>
-        </div>
-        <div v-else-if="!loading && projects.length === 0"
-             class="no-projects-message">
-          This course doesn't have any projects yet.
-        </div>
-        <div v-else v-for="(project, index) of projects"
-              :key="project.pk">
-          <single-project ref="single_project"
-                          :course="course"
-                          :existing_projects="projects"
-                          :project="project"
-                          :odd_index="index % 2 !== 0"
-                          @add_cloned_project="add_cloned_project($event)">
-          </single-project>
-        </div>
-      </div>
+      </ValidatedForm>
     </div>
+
+    <div v-if="loading" class="loading-wrapper loading-medium">
+      <i class="loading-centered fa fa-spinner fa-pulse"></i>
+    </div>
+    <div v-else-if="!loading && projects.length === 0"
+          class="no-projects-message">
+      This course has no projects.
+    </div>
+    <single-project v-else v-for="(project, index) of projects"
+                    :key="project.pk"
+                    ref="single_project"
+                    :course="course"
+                    :project="project"
+                    :odd_index="index % 2 !== 0">
+    </single-project>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-import { Course, Project } from 'ag-client-typescript';
+import { Course, Project, ProjectObserver } from 'ag-client-typescript';
 
 import APIErrors from "@/components/api_errors.vue";
 import SingleProject from '@/components/course_admin/manage_projects/single_project.vue';
 import Tooltip from '@/components/tooltip.vue';
 import ValidatedForm from '@/components/validated_form.vue';
 import ValidatedInput from '@/components/validated_input.vue';
-import { handle_api_errors_async } from '@/utils';
+import { BeforeDestroy, Created } from '@/lifecycle';
+import { handle_api_errors_async, toggle } from '@/utils';
 import { is_not_empty } from '@/validators';
 
 @Component({
@@ -82,8 +70,8 @@ import { is_not_empty } from '@/validators';
     ValidatedInput
   }
 })
-export default class ManageProjects extends Vue {
-
+export default class ManageProjects extends Vue implements ProjectObserver,
+                                                           Created, BeforeDestroy {
   @Prop({required: true, type: Course})
   course!: Course;
 
@@ -93,50 +81,51 @@ export default class ManageProjects extends Vue {
   projects: Project[] = [];
   new_project_name = "";
   new_project_name_is_valid = false;
-  d_course!: Course;
 
   d_adding_project = false;
 
   async created() {
-    this.d_course = this.course;
-    this.projects = await Project.get_all_from_course(this.d_course.pk);
+    this.projects = await Project.get_all_from_course(this.course.pk);
     this.loading = false;
+    Project.subscribe(this);
+  }
+
+  beforeDestroy() {
+    Project.unsubscribe(this);
   }
 
   sort_projects() {
-    this.projects.sort((project_a: Project, project_b: Project) => {
-      if (project_a.name <= project_b.name) {
-        return -1;
-      }
-      return 1;
-    });
-  }
-
-  add_cloned_project(new_project: Project) {
-    this.projects.push(new_project);
-    this.sort_projects();
+    this.projects.sort((first, second) => first.name.localeCompare(second.name));
   }
 
   @handle_api_errors_async(handle_add_project_error)
-  async add_project() {
-    try {
+  add_project() {
+    return toggle(this, 'd_adding_project', async () => {
       this.d_adding_project = true;
       this.new_project_name.trim();
       let new_project: Project = await Project.create(
-        this.d_course.pk, {name: this.new_project_name}
+        this.course.pk, {name: this.new_project_name}
       );
       this.projects.push(new_project);
       this.sort_projects();
       this.new_project_name = "";
       (<ValidatedInput> this.$refs.new_project_name).reset_warning_state();
+    });
+  }
+
+  update_project_created(new_project: Project) {
+    if (new_project.course === this.course.pk) {
+      this.projects.push(new_project);
+      this.sort_projects();
     }
-    finally {
-      this.d_adding_project = false;
-    }
+  }
+
+  update_project_changed(project: Project): void {
   }
 }
 
 export function handle_add_project_error(component: ManageProjects, error: unknown) {
+  console.log('WAAAA');
   (<APIErrors> component.$refs.api_errors).show_errors_from_response(error);
 }
 
@@ -144,74 +133,31 @@ export function handle_add_project_error(component: ManageProjects, error: unkno
 
 <style scoped lang="scss">
 @import '@/styles/button_styles.scss';
+@import '@/styles/forms.scss';
+@import '@/styles/loading.scss';
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+.loading-wrapper {
+  margin-top: 2rem;
+}
 
 .no-projects-message {
-  padding: 8px 0;
+  padding: .5rem 0;
+  color: darken($stormy-gray-dark, 10%);
 }
 
-.loading-projects {
-  padding: 6px 0;
-  font-size: 20px;
-  color: $ocean-blue;
-}
-
-#new-project-space {
-  margin: 0 5%;
-  width: 90%;
-}
-
-#new-project-label, #existing-projects-label {
-  font-size: 20px;
-  font-weight: 600;
+#manage-projects-component {
+  max-width: 500px;
 }
 
 #new-project-label {
-  margin: 10px 0 8px 0;
-}
-
-.add-project-button {
-  @extend .green-button;
-}
-
-.add-project-button {
-  margin: 15px 0 20px 0;
-}
-
-#existing-projects-side {
-  margin: 0 5%;
-  width: 90%;
-}
-
-#existing-projects-label {
-  margin: 40px 0 8px 0;
-}
-
-@media only screen and (min-width: 960px) {
-  #manage-projects-component {
-    margin: 0 2.5%;
-  }
-
-  #new-project-side {
-    display: inline-block;
-    width: 40%;
-    max-width: 500px;
-    box-sizing: border-box;
-    padding-right: 10px;
-  }
-
-  #new-project-space {
-    margin: 0;
-  }
-
-  #existing-projects-side {
-    display: inline-block;
-    margin: 0;
-    vertical-align: top;
-    width: 60%;
-  }
-
-  #existing-projects-label {
-    margin: 10px 0 12px 0;
-  }
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-bottom: .5rem;
 }
 </style>
--- tests/test_course_admin/test_manage_projects/test_manage_projects.ts
+++ tests/test_course_admin/test_manage_projects/test_manage_projects.ts
@@ -1,20 +1,22 @@
-import { config, mount, Wrapper } from '@vue/test-utils';
+import { config, Wrapper } from '@vue/test-utils';
 
-import { Course, HttpError, Project, Semester, UltimateSubmissionPolicy, User } from 'ag-client-typescript';
+import { Course, HttpError, Project, User } from 'ag-client-typescript';
 import * as sinon from 'sinon';
 
-import APIErrors from '@/componenets/api_errors.vue';
+import APIErrors from '@/components/api_errors.vue';
 import ManageProjects from '@/components/course_admin/manage_projects/manage_projects.vue';
 import SingleProject from '@/components/course_admin/manage_projects/single_project.vue';
 import ValidatedInput from '@/components/validated_input.vue';
 
-import { set_global_current_user } from '@/tests/data_utils';
+import * as data_ut from '@/tests/data_utils';
+import { managed_mount } from '@/tests/setup';
+import { find_by_name, set_validated_input_text, wait_until } from '@/tests/utils';
 
 beforeAll(() => {
     config.logModifiedComponents = false;
 });
 
-describe('ManageProjects.vue', () => {
+describe('Manage projects tests', () => {
     let component: ManageProjects;
     let wrapper: Wrapper<ManageProjects>;
     let user: User;
@@ -25,181 +27,43 @@ describe('ManageProjects.vue', () => {
     let project_1: Project;
     let project_2: Project;
     let projects: Project[];
-    let newly_cloned_project_1: Project;
-    let original_match_media: (query: string) => MediaQueryList;
 
-    beforeEach(() => {
-        original_match_media = window.matchMedia;
-        Object.defineProperty(window, "matchMedia", {
-            value: jest.fn(() => {
-                return {matches: true};
-            })
-        });
+    beforeEach(async () => {
+        user = data_ut.make_user();
 
-        user = new User({
-            pk: 1, username: 'ashberg', first_name: 'Ashley',
-            last_name: 'IceBerg', email: 'iceberg@umich.edu',
-            is_superuser: false
-        });
+        current_course = data_ut.make_course();
+        another_course = data_ut.make_course();
 
-        current_course = new Course({
-            pk: 1, name: 'EECS 280', semester: Semester.winter, year: 2019, subtitle: '',
-            num_late_days: 0, allowed_guest_domain: '', last_modified: ''
-        });
+        project_1 = data_ut.make_project(current_course.pk, {name: 'Project 1'});
+        project_2 = data_ut.make_project(current_course.pk, {name: 'Project 2'});
 
-        another_course = new Course({
-            pk: 2, name: 'EECS 280', semester: Semester.winter, year: 2020, subtitle: '',
-            num_late_days: 0, allowed_guest_domain: '', last_modified: ''
-        });
-
-        project_1 = new Project({
-            pk: 10,
-            name: "Project 1 - Statistics",
-            last_modified: "today",
-            course: 1,
-            visible_to_students: true,
-            closing_time: null,
-            soft_closing_time: null,
-            disallow_student_submissions: true,
-            disallow_group_registration: true,
-            guests_can_submit: true,
-            min_group_size: 1,
-            max_group_size: 1,
-            submission_limit_per_day: null,
-            allow_submissions_past_limit: true,
-            groups_combine_daily_submissions: false,
-            submission_limit_reset_time: "",
-            submission_limit_reset_timezone: "",
-            num_bonus_submissions: 1,
-            total_submission_limit: null,
-            allow_late_days: true,
-            ultimate_submission_policy: UltimateSubmissionPolicy.best,
-            hide_ultimate_submission_fdbk: false,
-            instructor_files: [],
-            expected_student_files: [],
-            has_handgrading_rubric: false,
-        });
-
-        project_2 = new Project({
-            pk: 4,
-            name: "Project 2 - Images",
-            last_modified: "today",
-            course: 1,
-            visible_to_students: true,
-            closing_time: null,
-            soft_closing_time: null,
-            disallow_student_submissions: true,
-            disallow_group_registration: true,
-            guests_can_submit: true,
-            min_group_size: 1,
-            max_group_size: 1,
-            submission_limit_per_day: null,
-            allow_submissions_past_limit: true,
-            groups_combine_daily_submissions: false,
-            submission_limit_reset_time: "",
-            submission_limit_reset_timezone: "",
-            num_bonus_submissions: 1,
-            total_submission_limit: null,
-            allow_late_days: true,
-            ultimate_submission_policy: UltimateSubmissionPolicy.best,
-            hide_ultimate_submission_fdbk: false,
-            instructor_files: [],
-            expected_student_files: [],
-            has_handgrading_rubric: false,
-        });
-
-        new_project = new Project ({
-            pk: 5,
-            name: "New Project",
-            last_modified: "today",
-            course: 1,
-            visible_to_students: true,
-            closing_time: null,
-            soft_closing_time: null,
-            disallow_student_submissions: true,
-            disallow_group_registration: true,
-            guests_can_submit: true,
-            min_group_size: 1,
-            max_group_size: 1,
-            submission_limit_per_day: null,
-            allow_submissions_past_limit: true,
-            groups_combine_daily_submissions: false,
-            submission_limit_reset_time: "",
-            submission_limit_reset_timezone: "",
-            num_bonus_submissions: 1,
-            total_submission_limit: null,
-            allow_late_days: true,
-            ultimate_submission_policy: UltimateSubmissionPolicy.best,
-            hide_ultimate_submission_fdbk: false,
-            instructor_files: [],
-            expected_student_files: [],
-            has_handgrading_rubric: false,
-        });
+        new_project = data_ut.make_project(current_course.pk, {name: 'Project 0'});
 
         projects = [project_1, project_2];
-
-        newly_cloned_project_1 = new Project({
-            pk: 12,
-            name: "george_cloney.cpp",
-            last_modified: "today",
-            course: 1,
-            visible_to_students: true,
-            closing_time: null,
-            soft_closing_time: null,
-            disallow_student_submissions: true,
-            disallow_group_registration: true,
-            guests_can_submit: true,
-            min_group_size: 1,
-            max_group_size: 1,
-            submission_limit_per_day: null,
-            allow_submissions_past_limit: true,
-            groups_combine_daily_submissions: false,
-            submission_limit_reset_time: "",
-            submission_limit_reset_timezone: "",
-            num_bonus_submissions: 1,
-            total_submission_limit: null,
-            allow_late_days: true,
-            ultimate_submission_policy: UltimateSubmissionPolicy.best_with_normal_fdbk,
-            hide_ultimate_submission_fdbk: false,
-            instructor_files: [],
-            expected_student_files: [],
-            has_handgrading_rubric: false,
-        });
-
         courses = [current_course, another_course];
 
-        set_global_current_user(user);
+        data_ut.set_global_current_user(user);
         sinon.stub(user, 'courses_is_admin_for').returns(Promise.resolve(courses));
         sinon.stub(Project, 'get_all_from_course').returns(Promise.resolve(projects));
 
-        wrapper = mount(ManageProjects, {
+        wrapper = managed_mount(ManageProjects, {
             propsData: {
                 course: current_course
             },
             stubs: ['router-link']
         });
         component = wrapper.vm;
-    });
-
-    afterEach(() => {
-        sinon.restore();
-
-        Object.defineProperty(window, "matchMedia", {
-            value: original_match_media
-        });
-
-        if (wrapper.exists()) {
-            wrapper.destroy();
-        }
+        await wrapper.vm.$nextTick();
     });
 
     test('Existing projects get fetched (sorted in http response)', async () => {
-        await component.$nextTick();
+        expect(wrapper.vm.projects).toEqual(projects);
+        expect(wrapper.vm.projects[0]).toEqual(project_1);
+        expect(wrapper.vm.projects[1]).toEqual(project_2);
 
-        expect(component.d_course).toEqual(current_course);
-        expect(component.projects).toEqual(projects);
-        expect(component.projects[0]).toEqual(project_1);
-        expect(component.projects[1]).toEqual(project_2);
+        let rendered_projects = wrapper.findAll({name: 'SingleProject'});
+        expect((<SingleProject> rendered_projects.at(0).vm).project).toEqual(project_1);
+        expect((<SingleProject> rendered_projects.at(1).vm).project).toEqual(project_2);
     });
 
     test('New project name cannot be an empty string', async () => {
@@ -209,14 +73,14 @@ describe('ManageProjects.vue', () => {
         let new_project_name = wrapper.find({ref: 'new_project_name'}).find('#input');
         (<HTMLInputElement> new_project_name.element).value = "   ";
         new_project_name.trigger('input');
-        await component.$nextTick();
+        await wrapper.vm.$nextTick();
 
         expect(validated_input.is_valid).toBe(false);
         expect(wrapper.find('.add-project-button').is('[disabled]')).toBe(true);
     });
 
     test('A project can be created and then displayed in the list of projects', async () => {
-        expect(component.projects.length).toEqual(2);
+        expect(wrapper.vm.projects.length).toEqual(2);
 
         let validated_input = <ValidatedInput> wrapper.find({ref: "new_project_name"}).vm;
         expect(validated_input.is_valid).toBe(false);
@@ -224,7 +88,7 @@ describe('ManageProjects.vue', () => {
         let new_project_name = wrapper.find({ref: 'new_project_name'}).find('#input');
         (<HTMLInputElement> new_project_name.element).value = new_project.name;
         new_project_name.trigger('input');
-        await component.$nextTick();
+        await wrapper.vm.$nextTick();
 
         expect(validated_input.is_valid).toBe(true);
 
@@ -232,31 +96,20 @@ describe('ManageProjects.vue', () => {
             Promise.resolve(new_project)
         );
         wrapper.find({ref: 'new_project_form'}).trigger('submit');
-        await component.$nextTick();
+        await wrapper.vm.$nextTick();
 
         expect(create_project_stub.firstCall.calledWith(
             current_course.pk, { name: new_project.name }
         )).toBe(true);
-        expect(component.projects.length).toEqual(3);
-        expect(component.projects[0]).toEqual(new_project);
-        expect(component.projects[1]).toEqual(project_1);
-        expect(component.projects[2]).toEqual(project_2);
+        expect(wrapper.vm.projects.length).toEqual(3);
+        expect(wrapper.vm.projects[0]).toEqual(new_project);
+        expect(wrapper.vm.projects[1]).toEqual(project_1);
+        expect(wrapper.vm.projects[2]).toEqual(project_2);
     });
 
-    test('New project name must be unique among projects in the same course - violates ' +
-         'condition',
-         async () => {
-        expect(component.projects.length).toEqual(2);
-
-        let validated_input = <ValidatedInput> wrapper.find({ref: "new_project_name"}).vm;
-        expect(validated_input.is_valid).toBe(false);
-
-        let new_project_name = wrapper.find({ref: 'new_project_name'}).find('#input');
-        (<HTMLInputElement> new_project_name.element).value = project_1.name;
-        new_project_name.trigger('input');
-        await component.$nextTick();
-
-        expect(validated_input.is_valid).toBe(true);
+    test('Create project API errors handled', async () => {
+        set_validated_input_text(wrapper.find({ref: "new_project_name"}), project_1.name);
+        await wrapper.vm.$nextTick();
 
         let create_project_stub = sinon.stub(Project, 'create').returns(
             Promise.reject(
@@ -267,44 +120,33 @@ describe('ManageProjects.vue', () => {
         );
 
         wrapper.find({ref: 'new_project_form'}).trigger('submit');
-        await component.$nextTick();
+        expect(await wait_until(wrapper, w => !w.vm.d_adding_project)).toBe(true);
 
-        let api_errors = <APIErrors> wrapper.find({ref: 'api_errors'}).vm;
-        // Note: api_errors.d_api_errors is flagged by the compiler here with error message
-        // "Property 'd_api_errors' does not exist on type 'Vue'." It's unclear why that
-        // is happening, so we'll access it through $data for now.
         expect(create_project_stub.calledOnce).toBe(true);
-        expect(api_errors.$data.d_api_errors.length).toBe(1);
+        let api_errors = find_by_name<APIErrors>(wrapper, 'APIErrors');
+        expect(api_errors.vm.d_api_errors.length).toEqual(1);
     });
 
     test('A project can be cloned to the current course', async () => {
-        expect(component.projects.length).toEqual(2);
+        expect(wrapper.findAll({name: 'SingleProject'}).length).toEqual(2);
 
-        let project_to_clone = <SingleProject> wrapper.findAll(
-            {ref: 'single_project'}
-        ).at(0).vm;
+        let clone = data_ut.make_project(current_course.pk, {name: 'Cloney'});
+        Project.notify_project_created(clone);
+        await wrapper.vm.$nextTick();
 
-        wrapper.findAll('.copier').at(0).trigger('click');
-        await component.$nextTick();
+        expect(wrapper.findAll({name: 'SingleProject'}).length).toEqual(3);
+        expect(wrapper.vm.projects[0]).toEqual(clone);
+        expect(wrapper.vm.projects[1]).toEqual(project_1);
+        expect(wrapper.vm.projects[2]).toEqual(project_2);
+    });
 
-        project_to_clone.cloned_project_name = newly_cloned_project_1.name;
-        await component.$nextTick();
+    test('Cloned project comes from different course', async () => {
+        expect(wrapper.findAll({name: 'SingleProject'}).length).toEqual(2);
 
-        expect(wrapper.find('.clone-project-button').is('[disabled]')).toBe(false);
+        let clone = data_ut.make_project(another_course.pk, {name: 'Cloney'});
+        Project.notify_project_created(clone);
+        await wrapper.vm.$nextTick();
 
-        let copy_to_course_stub = sinon.stub(project_1, 'copy_to_course').returns(
-            Promise.resolve(newly_cloned_project_1)
-        );
-
-        wrapper.find('.clone-project-button').trigger('click');
-        await component.$nextTick();
-
-        expect(copy_to_course_stub.firstCall.calledWith(
-            current_course.pk, newly_cloned_project_1.name)
-        ).toBe(true);
-        expect(component.projects.length).toEqual(3);
-        expect(component.projects[0]).toEqual(project_1);
-        expect(component.projects[1]).toEqual(project_2);
-        expect(component.projects[2]).toEqual(newly_cloned_project_1);
+        expect(wrapper.findAll({name: 'SingleProject'}).length).toEqual(2);
     });
 });
