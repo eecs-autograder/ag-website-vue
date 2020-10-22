b3b7d42bf4e16a785152a52e09e99399b57c146f
James Perretta <jameslp@umich.edu>
2020-10-22 09:46:35 -0400

    Changed the default SYSADMIN_CONTACT value to something generic rather than the help@autograder.io address.

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@ export const MAX_COMMAND_TIMEOUT = 90;
 export const MAX_STACK_SIZE_LIMIT = 100000000;  // 100 MB
 export const MAX_PROCESS_LIMIT = 150;
 
-export const SYSADMIN_CONTACT = 'help@autograder.io';
+export const SYSADMIN_CONTACT = 'your instructor or system administrator';
