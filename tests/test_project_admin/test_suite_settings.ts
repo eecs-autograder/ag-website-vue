d5adecb29a54f5e88dc803ae7a3221a7116d6b88
Cay Wagner <cowagner@umich.edu>
2020-12-13 16:46:55 -0500

    Add suite settings batch select tests

--- tests/test_project_admin/test_suite_settings.ts
+++ tests/test_project_admin/test_suite_settings.ts
@@ -395,4 +395,49 @@ describe('Field binding tests', () => {
 
         expect(emitted(wrapper, 'field_change')[0][0]).toEqual(wrapper.vm.d_suite);
     });
+
+    test('Adding instructor file using batch select', async () => {
+        expect(wrapper.vm.d_suite!.instructor_files_needed.length).toEqual(2);
+        
+        wrapper.findAll('.batch-select-button').at(0).trigger('click');
+        await wrapper.vm.$nextTick();
+
+        wrapper.findAll('.batch-select-card').at(2).trigger('click');
+        await wrapper.vm.$nextTick();
+
+        wrapper.find('.modal-confirm-button').trigger('click');
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.vm.d_suite!.instructor_files_needed.length).toEqual(3);
+    });
+
+    test('Removing instructor file using batch select', async () => {
+        expect(wrapper.vm.d_suite!.instructor_files_needed.length).toEqual(2);
+        
+        wrapper.findAll('.batch-select-button').at(0).trigger('click');
+        await wrapper.vm.$nextTick();
+
+        wrapper.findAll('.batch-select-card').at(0).trigger('click');
+        await wrapper.vm.$nextTick();
+
+        wrapper.find('.modal-confirm-button').trigger('click');
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.vm.d_suite!.instructor_files_needed.length).toEqual(1);
+    });
+
+    test('Cancelling instructor file batch select preserves state', async () => {
+        expect(wrapper.vm.d_suite!.instructor_files_needed.length).toEqual(2);
+        
+        wrapper.findAll('.batch-select-button').at(0).trigger('click');
+        await wrapper.vm.$nextTick();
+
+        wrapper.findAll('.batch-select-card').at(0).trigger('click');
+        await wrapper.vm.$nextTick();
+
+        wrapper.find('.modal-cancel-button').trigger('click');
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.vm.d_suite!.instructor_files_needed.length).toEqual(2);
+    });
 });
