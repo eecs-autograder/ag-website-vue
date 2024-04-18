d46580d8c21b55cfa8dfa8c188b7fe88bdcaba6d
normanqw <normanqw@umich.edu>
2024-04-17 20:36:49 -0400

    Primitively implemented feature
    Open eye for visible projects
    Crossed-out eye for not visible projects
    Eye should not be visible to students

--- src/components/course_view.vue
+++ src/components/course_view.vue
@@ -16,7 +16,7 @@
                       :to="`/web/project/${project.pk}`">
           <div class="project-name-container">
             <span class="left-text">{{project.name}}</span>
-            <span class="right-text tool-icon">
+            <span class="right-text tool-icon" v-if="!d_globals.user_roles.is_student">
               <i class="fa fa-eye" aria-hidden="true" v-if="project.visible_to_students"></i>
               <i class="fa fa-eye-slash" aria-hidden="true" v-else></i>
             </span>
