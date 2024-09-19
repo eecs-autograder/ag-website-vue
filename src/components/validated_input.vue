ea36aa404b05e071bafffeb817b7ce3397f65ef5
James Perretta <jameslp@umich.edu>
2024-09-19 15:27:47 -0400

    Added cypress config and 2 example tests for validated_input.vue.

--- src/components/validated_input.vue
+++ src/components/validated_input.vue
@@ -3,6 +3,7 @@
     <div class="validated-input-wrapper">
       <slot name="prefix"> </slot>
       <input class="input"
+             data-testid="input"
              v-if="num_rows === 1"
              :style="input_style"
              :class="{
@@ -18,6 +19,7 @@
                 :rows="num_rows"
                 :style="input_style"
                 class="input"
+                data-testid="input"
                 :class="{
                  'error-input' : input_style === '' && show_errors
                 }"
