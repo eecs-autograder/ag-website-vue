21af0d2127c8c19ff60ac789b145781a6b3b6bee
Matt Mayfield <mattmayf@umich.edu>
2024-10-11 17:07:05 -0400

    Fix static_analysis with more specific patterns, delete .eslintignore

--- static_analysis.bash
+++ static_analysis.bash
@@ -2,17 +2,11 @@ set -ex
 
 npx eslint \
     'cypress/**/*.ts' \
-    '**/*.cy.ts'
+    'src/**/*.cy.ts'
 
-# hack because .prettierignore doesn't work as expected.
-# See https://github.com/prettier/prettier/issues/16744
-# There are files with restricted privileges in test_stack that will
-# cause prettier to fail unless we move it.
-mv cypress/e2e/test_stack /tmp/
 npx prettier --check \
     './*.js' \
     'cypress/**/*.ts' \
-    '**/*.cy.ts'
-mv /tmp/test_stack cypress/e2e/
+    'src/**/*.cy.ts'
 
 ./check_subscribe_unsubscribe.py 'src/**/*.vue'
