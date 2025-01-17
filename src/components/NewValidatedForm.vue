4a78677ec8b5e06b543a03c6fc73c4746cc5fedb
James Perretta <jameslp@umich.edu>
2025-01-17 15:56:27 -0500

    Rename aspects of use_validation.ts to more clearly reflect the relationship between the different pieces.

--- src/components/NewValidatedForm.vue
+++ src/components/NewValidatedForm.vue
@@ -8,7 +8,7 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 
-import { use_validation_observer, ObserverEmitTypes } from '@/composables/use_validation';
+import { use_validation_group, ValidatorEmitTypes } from '@/composables/use_validation';
 
 // TODO: A limitation of the vue SFC compiler is that emit types need to be known
 // at compile time. As of 3.3, however, imported types can be used to define emits.
@@ -23,7 +23,7 @@ type EmitTypes = {
 }
 const emit = defineEmits<EmitTypes>();
 
-const is_valid = use_validation_observer(emit);
+const is_valid = use_validation_group(emit);
 
 const handle_submit = (e: Event) => {
   e.preventDefault;
