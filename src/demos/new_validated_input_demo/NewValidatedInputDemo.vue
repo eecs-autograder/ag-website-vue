f0091a980b71b0d4c3169e268e69d1914b3228a4
Matt Mayfield <mattmayf@umich.edu>
2025-01-21 15:01:34 -0500

    Add ValidatedTextAreaInput component

--- src/demos/new_validated_input_demo/NewValidatedInputDemo.vue
+++ src/demos/new_validated_input_demo/NewValidatedInputDemo.vue
@@ -67,10 +67,42 @@
           <span style="font-weight: bold">{{is_valid_2}}</span>
         </p>
         <p>
-          Last emitted value (Note: it will not be updated until inputted value
-          is valid. Also note: warnings will show up if you click on the input
-          and then click away, they will only show up once you start editing):
+          Last emitted value:
           <span style="font-weight: bold">{{val_2}}</span>
+        </p>
+      </div>
+    </div>
+
+    <div id="validated-text-area" style="padding: 10px;">
+      <h1>Validated Text Area</h1>
+
+      <div id="validated-text-area-1">
+        <small>Validated Text Area 1</small>
+        <hr />
+        <p>
+          The below input must be at least 2 lines, and contain the words "Mario"
+          or "Luigi" (case insensitive).
+        </p>
+
+        <validated-text-area-input
+          v-model="val_3"
+          :num_rows="2"
+          :validators="[
+            two_or_more_lines,
+            has_luigi_or_mario
+          ]"
+          placeholder="Write a story about Mario or Luigi..."
+          @validity_changed="(new_is_valid) => { is_valid_3 = new_is_valid; }"
+        >
+          <template v-slot:label> Text Area </template>
+        </validated-text-area-input>
+        <p>
+          is_valid() result:
+          <span style="font-weight: bold">{{is_valid_3}}</span>
+        </p>
+        <p>
+          Last emitted value:
+          <span style="font-weight: bold">{{val_3}}</span>
         </p>
       </div>
     </div>
@@ -82,28 +114,28 @@
         <small>Validated Pair 1</small>
         <hr />
         <p>
-          The below pair of inputs must both be positive integers that sum to
-          a number that is divisible by 4. Note that the custom component has a
-          validator that forms a closure with the is_valid values of the two child
-          ValidateIntInput components to produce it's own error if both operands
-          are not valid. This would be necessary if this component were part of
-          a ValidatedForm, because emitted events don't bubble. Alternatively,
-          the component could listen for the validity_changed events of the child
-          components and re-emit them without creating another validator.
-
+          The below pair of inputs must both be positive integers that sum to a
+          number that is divisible by 4. Note that the custom component has a
+          validator that forms a closure with the is_valid values of the two
+          child ValidateIntInput components to produce it's own error if both
+          operands are not valid. This would be necessary if this component were
+          part of a ValidatedForm, because emitted events don't bubble.
+          Alternatively, the component could listen for the validity_changed
+          events of the child components and re-emit them without creating
+          another validator.
         </p>
 
         <paired-inputs
-          v-model="val_3"
-          @validity_changed="(new_is_valid) => { is_valid_3 = new_is_valid; }"
+          v-model="val_4"
+          @validity_changed="(new_is_valid) => { is_valid_4 = new_is_valid; }"
         />
         <p>
           is_valid() result:
-          <span style="font-weight: bold">{{is_valid_3}}</span>
+          <span style="font-weight: bold">{{is_valid_4}}</span>
         </p>
         <p>
           Last emitted value:
-          <span style="font-weight: bold">{{val_3}}</span>
+          <span style="font-weight: bold">{{val_4}}</span>
         </p>
       </div>
     </div>
@@ -112,6 +144,7 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+
 import ValidatedTextInput, {
   TextInputValidator,
   make_min_length_validator,
@@ -122,6 +155,9 @@ import ValidatedIntInput, {
   make_min_validator,
   make_max_validator
 } from "@/components/validated_input/ValidatedIntInput.vue";
+import ValidatedTextAreaInput, {
+  TextAreaInputValidator
+} from "@/components/validated_input/ValidatedTextAreaInput.vue";
 import PairedInputs from "./PairedInputs.vue";
 
 /**** ValidatedTextInput demo ****/
@@ -169,8 +205,39 @@ const even_number: IntInputValidator = (input: number) => {
 }
 
 /*** PairedInput demo ****/
-const val_3 = ref();
-const is_valid_3 = ref();
+const val_3 = ref("");
+const is_valid_3 = ref(true)
+
+const has_luigi_or_mario: TextAreaInputValidator = (input: string) => {
+  const text = input.toLowerCase();
+  if (!(text.includes('luigi') || text.includes('mario'))) {
+    return {
+      is_valid: false,
+      error_msg: 'Does not contain Mario or Luigi. Proposterous!'
+    }
+  }
+  else {
+    return {
+      is_valid: true
+    }
+  }
+}
+const two_or_more_lines: TextAreaInputValidator = (input: string) => {
+  const num_lines = input.split('\n');
+  if (num_lines.length >= 2) {
+    return { is_valid: true };
+  }
+  else {
+    return {
+      is_valid: false,
+      error_msg: "Input must two or more lines"
+    }
+  }
+}
+
+/*** PairedInput demo ****/
+const val_4 = ref();
+const is_valid_4 = ref();
 </script>
 
 <style scoped lang="scss">
