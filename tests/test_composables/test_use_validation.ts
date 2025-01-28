344c04d2f79ea48dc8ba2c81ba3f3a03a358c0b0
Matt Mayfield <mattmayf@umich.edu>
2025-01-28 12:32:45 -0500

    Add use_validation_group tests

--- tests/test_composables/test_use_validation.ts
+++ tests/test_composables/test_use_validation.ts
@@ -1,9 +1,18 @@
 import { mount } from "@vue/test-utils";
 import { vi } from "vitest";
-import Vue, { ComponentPublicInstance, h, ref } from "vue";
+import Vue, {
+  ComponentPublicInstance,
+  h,
+  inject,
+  onBeforeUnmount,
+  onMounted,
+  ref,
+  Ref,
+} from "vue";
 
 import {
   use_validation,
+  use_validation_group,
   UseValidationParams,
   ValidatorFuncType,
 } from "@/composables/use_validation";
@@ -14,13 +23,13 @@ describe("use_validation", () => {
     const register = vi.fn((_) => uid);
     const unregister = vi.fn((_) => undefined);
 
-    make_use_validation_comp({
+    const comp = make_use_validation_comp({
       input: ref(""),
       validators: [],
     });
 
     const wrapper = mount(
-      make_use_validation_parent({
+      make_use_validation_parent(comp, {
         register,
         unregister,
       }),
@@ -33,33 +42,33 @@ describe("use_validation", () => {
   });
 
   test("emits initial valid input value", () => {
-    const child = make_use_validation_comp({
+    const comp = make_use_validation_comp({
       input: ref(""),
       validators: [],
     });
 
-    const wrapper = mount(child);
+    const wrapper = mount(comp);
     expect(wrapper.emitted("input")?.[0]).toEqual([""]);
   });
 
   test("does not emit initial invalid input value", () => {
-    const child = make_use_validation_comp({
+    const comp = make_use_validation_comp({
       input: ref(""),
       validators: [(_) => ({ is_valid: false, error_msg: "invalid" })],
     });
 
-    const wrapper = mount(child);
+    const wrapper = mount(comp);
     expect(wrapper.emitted()).not.toHaveProperty("input");
   });
 
   test("emits input value when changed to a valid value", async () => {
     const input = ref("");
-    const child = make_use_validation_comp({
+    const comp = make_use_validation_comp({
       input: input,
       validators: [],
     });
 
-    const wrapper = mount(child);
+    const wrapper = mount(comp);
     input.value = "valid!";
 
     await Vue.nextTick();
@@ -70,12 +79,12 @@ describe("use_validation", () => {
 
   test("does not emit input value when changed to an invalid value", async () => {
     const input = ref("");
-    const child = make_use_validation_comp({
+    const comp = make_use_validation_comp({
       input: input,
       validators: [default_validator],
     });
 
-    const wrapper = mount(child);
+    const wrapper = mount(comp);
     input.value = "invalid!";
 
     await Vue.nextTick();
@@ -88,13 +97,13 @@ describe("use_validation", () => {
 
   test("updates return values when input is changed to an invalid value", async () => {
     const input = ref("");
-    const child = make_use_validation_comp({
+    const comp = make_use_validation_comp({
       input: input,
       validators: [default_validator],
     });
 
-    const wrapper = mount(child);
-    const vm = wrapper.vm as unknown as ComponentInstance;
+    const wrapper = mount(comp);
+    const vm = wrapper.vm as unknown as UseValidationCompInstance;
 
     expect(vm.is_valid).toBe(true);
     expect(vm.errors).toEqual([]);
@@ -108,13 +117,13 @@ describe("use_validation", () => {
 
   test("updates return values when input is changed to a valid value", async () => {
     const input = ref("invalid!");
-    const child = make_use_validation_comp({
+    const comp = make_use_validation_comp({
       input: input,
       validators: [default_validator],
     });
 
-    const wrapper = mount(child);
-    const vm = wrapper.vm as unknown as ComponentInstance;
+    const wrapper = mount(comp);
+    const vm = wrapper.vm as unknown as UseValidationCompInstance;
 
     expect(vm.is_valid).toBe(false);
     expect(vm.errors).toEqual(["invalid!"]);
@@ -128,7 +137,7 @@ describe("use_validation", () => {
 
   test("updates return values correctly with multiple validators", async () => {
     const input = ref("");
-    const child = make_use_validation_comp({
+    const comp = make_use_validation_comp({
       input: input,
       validators: [
         default_validator,
@@ -155,8 +164,8 @@ describe("use_validation", () => {
       ],
     });
 
-    const wrapper = mount(child);
-    const vm = wrapper.vm as unknown as ComponentInstance;
+    const wrapper = mount(comp);
+    const vm = wrapper.vm as unknown as UseValidationCompInstance;
 
     expect(vm.is_valid).toBe(false);
     expect(vm.errors).toEqual(["empty string"]);
@@ -184,7 +193,7 @@ describe("use_validation", () => {
 
   test("updates return values when parsing fails", async () => {
     const input = ref(["hello", "world"]);
-    const child = make_use_validation_comp<string[]>({
+    const comp = make_use_validation_comp<string[]>({
       input: input,
       validators: [],
       parser: (value) => {
@@ -201,8 +210,8 @@ describe("use_validation", () => {
       },
     });
 
-    const wrapper = mount(child);
-    const vm = wrapper.vm as unknown as ComponentInstance;
+    const wrapper = mount(comp);
+    const vm = wrapper.vm as unknown as UseValidationCompInstance;
 
     expect(vm.is_valid).toBe(true);
     expect(vm.errors).toEqual([]);
@@ -216,7 +225,7 @@ describe("use_validation", () => {
 
   test("updates return values when parsing succeeds", async () => {
     const input = ref(["hello", "world"]);
-    const child = make_use_validation_comp<string[]>({
+    const comp = make_use_validation_comp<string[]>({
       input: input,
       validators: [],
       parser: (value) => {
@@ -233,8 +242,8 @@ describe("use_validation", () => {
       },
     });
 
-    const wrapper = mount(child);
-    const vm = wrapper.vm as unknown as ComponentInstance;
+    const wrapper = mount(comp);
+    const vm = wrapper.vm as unknown as UseValidationCompInstance;
 
     expect(vm.is_valid).toBe(false);
     expect(vm.errors).toEqual(["less than 3"]);
@@ -247,11 +256,165 @@ describe("use_validation", () => {
   });
 });
 
+describe("use_validation_group", () => {
+  it("provides register and unregister functions", () => {
+    const child = make_use_validation_group_child(ref(true));
+
+    // sanity check -- we expect mounting the child without a parent that uses
+    // use_validation_group to throw an error when injecting
+    expect(() => {
+      mount(child);
+    }).toThrow();
+
+    // this should throw if use_validation_group doesn't provide register/unregister
+    const wrapper = mount(make_use_validation_group_comp(child));
+
+    // sanity check -- make sure the child actually mounted
+    expect(wrapper.findComponent(child).exists()).toBe(true);
+
+    wrapper.destroy();
+  });
+
+  it("emits validity_changed with initial validity", () => {
+    let child_is_valid = ref(true);
+    let child = make_use_validation_group_child(child_is_valid);
+    let wrapper = mount(make_use_validation_group_comp(child));
+    expect(wrapper.emitted("validity_changed")?.[0]).toEqual([true]);
+
+    child_is_valid = ref(false);
+    child = make_use_validation_group_child(child_is_valid);
+    wrapper = mount(make_use_validation_group_comp(child));
+    expect(wrapper.emitted("validity_changed")?.[0]).toEqual([false]);
+  });
+
+  it("emits validity_changed with changes in validity", async () => {
+    const child_is_valid = ref(true);
+    const child = make_use_validation_group_child(child_is_valid);
+    const wrapper = mount(make_use_validation_group_comp(child));
+
+    child_is_valid.value = false;
+    await Vue.nextTick();
+    expect(wrapper.emitted("validity_changed")?.[1]).toEqual([false]);
+
+    child_is_valid.value = true;
+    await Vue.nextTick();
+    expect(wrapper.emitted("validity_changed")?.[2]).toEqual([true]);
+  });
+
+  it("does not emit validity_changed when group validity doesn't change", async () => {
+    const child_1_is_valid = ref(false);
+    const child_2_is_valid = ref(false);
+    const child_1 = make_use_validation_group_child(child_1_is_valid);
+    const child_2 = make_use_validation_group_child(child_2_is_valid);
+    const wrapper = mount(make_use_validation_group_comp(child_1, child_2));
+
+    child_1_is_valid.value = true;
+    await Vue.nextTick();
+    expect(wrapper.emitted("validity_changed")?.length).toEqual(1);
+
+    child_1_is_valid.value = false;
+    await Vue.nextTick();
+    expect(wrapper.emitted("validity_changed")?.length).toEqual(1);
+  });
+
+  it("returns correct initial value", () => {
+    let wrapper = mount(make_use_validation_group_comp());
+    let vm = wrapper.vm as unknown as UseValidationGroupCompInstance;
+    expect(vm.all_valid).toBe(true);
+
+    wrapper = mount(
+      make_use_validation_group_comp(
+        make_use_validation_group_child(ref(true)),
+      ),
+    );
+    vm = wrapper.vm as unknown as UseValidationGroupCompInstance;
+    expect(vm.all_valid).toBe(true);
+
+    wrapper = mount(
+      make_use_validation_group_comp(
+        make_use_validation_group_child(ref(false)),
+      ),
+    );
+    vm = wrapper.vm as unknown as UseValidationGroupCompInstance;
+    expect(vm.all_valid).toBe(false);
+  });
+
+  it("updates return value when a child becomes invalid", async () => {
+    const child_is_valid = ref(true);
+    const child = make_use_validation_group_child(child_is_valid);
+    const wrapper = mount(make_use_validation_group_comp(child));
+
+    child_is_valid.value = false;
+    await Vue.nextTick();
+    const vm = wrapper.vm as unknown as UseValidationGroupCompInstance;
+    expect(vm.all_valid).toBe(false);
+  });
+
+  it("updates return value when a child becomes valid", async () => {
+    const child_is_valid = ref(false);
+    const child = make_use_validation_group_child(child_is_valid);
+    const wrapper = mount(make_use_validation_group_comp(child));
+
+    child_is_valid.value = true;
+    await Vue.nextTick();
+    const vm = wrapper.vm as unknown as UseValidationGroupCompInstance;
+    expect(vm.all_valid).toBe(true);
+  });
+
+  it("updates return value correctly with multiple children", async () => {
+    const child_1_is_valid = ref(true);
+    const child_2_is_valid = ref(false);
+    const child_3_is_valid = ref(true);
+    const child_1 = make_use_validation_group_child(child_1_is_valid);
+    const child_2 = make_use_validation_group_child(child_2_is_valid);
+    const child_3 = make_use_validation_group_child(child_3_is_valid);
+
+    const wrapper = mount(
+      make_use_validation_group_comp(child_1, child_2, child_3),
+    );
+    const vm = wrapper.vm as unknown as UseValidationGroupCompInstance;
+    expect(vm.all_valid).toBe(false); // true, false, true
+
+    child_2_is_valid.value = true; // true, true, true
+    await Vue.nextTick();
+    expect(vm.all_valid).toBe(true);
+
+    child_1_is_valid.value = false;
+    child_3_is_valid.value = false; // false, true, false
+    await Vue.nextTick();
+    expect(vm.all_valid).toBe(false);
+
+    child_1_is_valid.value = true; // true, true, false
+    await Vue.nextTick();
+    expect(vm.all_valid).toBe(false);
+  });
+
+  it("isn't affected by unregistered children", async () => {
+    const child_is_valid = ref(false);
+    const child = make_use_validation_group_child(child_is_valid);
+    const wrapper = mount(make_use_validation_group_comp(child));
+    const vm = wrapper.vm as unknown as UseValidationGroupCompInstance;
+
+    // destroy child, which should unregister
+    wrapper.findComponent(child).destroy();
+    await Vue.nextTick();
+    expect(vm.all_valid).toBe(true);
+  });
+});
+
+/*******************************************************************************
+ * use_validation test helpers
+ *******************************************************************************
+ */
+
 type LifecycleFuncs = {
   register?: (is_valid: boolean) => number;
   unregister?: (uid: number) => void;
 };
-function make_use_validation_parent(lifecycle_funcs?: LifecycleFuncs) {
+function make_use_validation_parent(
+  use_validation_comp: ReturnType<typeof make_use_validation_group_child>,
+  lifecycle_funcs?: LifecycleFuncs,
+) {
   const provided_funcs: Partial<LifecycleFuncs> = {};
 
   // If register or unregister is not provided, we expect default
@@ -266,7 +429,7 @@ function make_use_validation_parent(lifecycle_funcs?: LifecycleFuncs) {
   return Vue.extend({
     provide: provided_funcs,
     render() {
-      return h("div", [h("Child")]);
+      return h("div", [h(use_validation_comp)]);
     },
   });
 }
@@ -274,8 +437,8 @@ function make_use_validation_parent(lifecycle_funcs?: LifecycleFuncs) {
 function make_use_validation_comp<Input = string>(
   params: Omit<UseValidationParams<Input, string>, "emit">,
 ) {
-  const child = Vue.extend({
-    name: "Child",
+  return Vue.extend({
+    name: "UseValidationComp",
     setup(_, { emit }) {
       const { is_valid, errors } = use_validation<Input, string>({
         ...params,
@@ -287,13 +450,9 @@ function make_use_validation_comp<Input = string>(
       return h("div");
     },
   });
-
-  // register Child component
-  Vue.component("Child", child);
-  return child;
 }
 
-type ComponentInstance = ComponentPublicInstance & {
+type UseValidationCompInstance = ComponentPublicInstance & {
   is_valid: boolean;
   errors: string[];
 };
@@ -310,3 +469,63 @@ const default_validator: ValidatorFuncType<string> = (value: string) => {
   }
   return { is_valid: true };
 };
+
+/*******************************************************************************
+ * use_validation_group test helpers
+ *******************************************************************************
+ */
+type UseValidationGroupCompInstance = ComponentPublicInstance & {
+  all_valid: boolean;
+};
+
+function make_use_validation_group_comp(
+  ...children: ReturnType<typeof make_use_validation_group_child>[]
+) {
+  return Vue.extend({
+    setup(_, { emit }) {
+      const all_valid = use_validation_group(emit);
+      return { all_valid };
+    },
+    render() {
+      return h(
+        "div",
+        {}, // Props for the parent container if needed
+        children.map((ChildComp) => h(ChildComp)), // Dynamically render all children
+      );
+    },
+  });
+}
+
+function make_use_validation_group_child(is_valid?: Ref) {
+  return Vue.extend({
+    name: "ValidationChild",
+    setup() {
+      // Inject `register` and `unregister`. If they're not provided, it should throw.
+      const register: ((is_valid: Ref) => number) | undefined =
+        inject("register");
+      const unregister: ((uid: number) => void) | undefined =
+        inject("unregister");
+
+      if (!register || !unregister) {
+        throw new Error("inject called but register/unregister is undefined");
+      }
+
+      let uid: number | undefined;
+
+      onMounted(() => {
+        uid = register(is_valid ? is_valid : ref(true));
+      });
+
+      onBeforeUnmount(() => {
+        if (uid !== undefined) {
+          unregister(uid);
+        }
+      });
+
+      return {};
+    },
+    render() {
+      return h("div");
+    },
+  });
+}
