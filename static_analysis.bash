d5801dd2e9ba5ed63a4532ef589c8157cf40bc33
Matt Mayfield <mattmayf@umich.edu>
2025-01-31 13:13:07 -0500

    Fix static_analysis paths

--- static_analysis.bash
+++ static_analysis.bash
@@ -5,7 +5,7 @@ npx eslint \
     'src/composables/**/*.ts' \
     'src/components/validated_input/**/*.vue' \
     'src/demos/new_validated_input_demo/**/*.vue' \
-    'tests/test_components/test_validated_input/**/*.ts'
+    'tests/test_components/test_new_validated_input.ts'
 
 npx prettier --check \
     './*.js' \
@@ -15,6 +15,6 @@ npx prettier --check \
     './src/components/validated_input/**/*.vue' \
     'src/demos/new_validated_input_demo/**/*.vue' \
     'tests/test_composables/**/*.ts' \
-    'tests/test_components/test_validated_input/**/*.ts'
+    'tests/test_components/test_new_validated_input.ts'
 
 ./check_subscribe_unsubscribe.py 'src/**/*.vue'
