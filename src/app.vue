ba317920cb64112239a284bc67e11a49990e1d5f
James Perretta <perretta.james@gmail.com>
2026-04-09 23:04:59 -0400

    Revert user menu to hover behavior & set correct roles and aria-labels for that behavior. Change static dropdown to show/hide with visibility attribute instead of display so that it's always present in the DOM.

--- src/app.vue
+++ src/app.vue
@@ -75,28 +75,11 @@
               Sign In
             </button>
           </div>
-          <div v-else class="current-user-dropdown" @blur="d_user_menu_open = false">
-            <a
-              @click.prevent="d_user_menu_open = !d_user_menu_open"
-              @keypress.enter="d_user_menu_open = !d_user_menu_open"
-              @keypress.space="d_user_menu_open = !d_user_menu_open"
-              @keydown.esc="d_user_menu_open = false"
-              :aria-expanded="d_user_menu_open"
-              aria-label="open user menu"
-              role="button"
-              aria-controls="user-menu"
-              class="dropdown-header"
-              tabindex="0"
-            >
-              <i class="fas fa-user"></i>
-            </a>
-            <div
-              id="user-menu"
-              v-show="d_user_menu_open"
-              class="menu"
-              aria-label="user menu"
-              @keydown.esc="d_user_menu_open = false"
-            >
+          <div v-else class="current-user-dropdown">
+            <div class="dropdown-header" tabindex="0">
+              <i class="fas fa-user" role="img" aria-label="User"></i>
+            </div>
+            <div class="menu">
               <div class="greeting">Hi, {{globals.current_user.first_name}}!</div>
               <div class="signed-in-as">Signed in as:</div>
               <div class="username">{{globals.current_user.username}}</div>
@@ -222,8 +205,6 @@ export default class App extends Vue implements GlobalErrorsObserver, Created, B
   d_global_error_count = 0;
 
   d_loading = true;
-
-  d_user_menu_open = false;
 
   @handle_global_errors_async
   async created() {
@@ -361,7 +342,7 @@ $breadcrumb-font-size: 1.5rem;
 }
 
 .current-user-dropdown {
-  @include static-dropdown($open-on-hover: false, $orient-right: true);
+  @include static-dropdown($open-on-hover: true, $orient-right: true);
 
   &:hover {
     background-color: darken($banner-background-color, 5%);
@@ -377,14 +358,9 @@ $breadcrumb-font-size: 1.5rem;
 
     white-space: nowrap;
     cursor: default;
-
-    &:hover {
-      cursor: pointer;
-    }
   }
 
   .menu {
-    display: block;
     padding: .5rem;
     min-width: 250px;
   }
