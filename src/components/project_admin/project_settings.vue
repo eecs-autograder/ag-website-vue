7abbdc82190bdea3577523d168e1c07197b276f2
Matt Mayfield <mattmayf@umich.edu>
2026-05-01 14:01:14 -0400

    Accessibility updates for "Settings" tab of project admin page (#590)
    * Fix form and region accessibility
    
    * Put all tooltips inside label tags for consistency
    
    * Add aria-label to edit timezone button
    
    * Fix formatting
    
    * Fix e2e tests
    
    * Change unnecessary id attributes to data-testid attributes
    The new validated input components handle the id internally. It was
    misleading to have id props passed to them.
    
    * Add data-testid to elements for testing, remove id lookups in tests
    
    * Undo global ocean-blue color change
    
    * Add keybindings for link from reset timezone to new timezone field.
    
    * Remove unnecessary UIDs from IDs
    
    * Fix default value of max_errors_to_show prop in InputErrors.vue
    
    ---------
    
    Co-authored-by: James Perretta <jameslp@umich.edu>

--- src/components/project_admin/project_settings.vue
+++ src/components/project_admin/project_settings.vue
@@ -1,20 +1,21 @@
 <template>
-  <div id="project-settings-component">
+  <div id="project-settings-component" class="project-settings-component">
     <new-validated-form
       ref="project_settings_form"
       autocomplete="off"
       spellcheck="false"
+      aria-label="Project settings"
       @submit="save_project_settings"
       @update:is_valid="state.settings_form_is_valid = $event"
     >
       <div id="project-name-container" class="form-field-wrapper">
-        <label class="label"> Project Name </label>
         <validated-text-input
           ref="project_name_input"
           v-model="state.project.name"
           :validators="[is_not_empty]"
           input_style="max-width: 500px; width: 100%"
         >
+          <template v-slot:label>Project Name</template>
         </validated-text-input>
       </div>
 
@@ -24,7 +25,7 @@
           <div class="form-field-wrapper">
             <label class="label" for="soft-deadline">
               Soft Deadline
-              <tooltip width="medium" placement="top">
+              <tooltip width="large" placement="top">
                 The deadline shown to students.
               </tooltip>
             </label>
@@ -128,11 +129,11 @@
                 v-model="state.project.guests_can_submit"
               />
               Anyone with the link can submit
+              <tooltip width="large" placement="top">
+                This can be restricted to users with a specific email domain in
+                the course settings.
+              </tooltip>
             </label>
-            <tooltip width="large" placement="top">
-              This can be restricted to users with a specific email domain in
-              the course settings.
-            </tooltip>
           </div>
 
           <div class="checkbox-input-container">
@@ -144,11 +145,11 @@
                 v-model="state.project.disallow_student_submissions"
               />
               Disable submitting
+              <tooltip width="large" placement="top">
+                Temporarily prevent students from submitting (they can still see
+                their previous submissions).
+              </tooltip>
             </label>
-            <tooltip width="large" placement="top">
-              Temporarily prevent students from submitting (they can still see
-              their previous submissions).
-            </tooltip>
           </div>
 
           <div class="checkbox-input-container">
@@ -164,12 +165,12 @@
                 "
               />
               Publish final grades
+              <tooltip width="large" placement="top">
+                When the hard deadline has passed and scores are published,
+                students will see their final grade for the project on the
+                submit page.
+              </tooltip>
             </label>
-            <tooltip width="large" placement="top">
-              When the hard deadline has passed and scores are published,
-              students will see their final grade for the project on the submit
-              page.
-            </tooltip>
           </div>
         </fieldset>
       </div>
@@ -180,28 +181,30 @@
 
           <div class="form-field-wrapper">
             <div class="group-size-container">
-              <label class="label"> Min group size </label>
               <validated-int-input
-                id="min-group-size"
+                data-testid="min_group_size"
                 v-model="state.project.min_group_size"
                 :validators="[make_min_validator(1)]"
                 input_style="max-width: 80px;"
               >
+                <template v-slot:label> Min group size </template>
               </validated-int-input>
             </div>
 
             <div class="group-size-container">
-              <label class="label"> Max group size </label>
-              <tooltip width="large" placement="top">
-                When this is > 1, users will be prompted to register their group
-                members the first time they visit the project page.
-              </tooltip>
               <validated-int-input
-                id="max-group-size"
+                data-testid="max_group_size"
                 v-model="state.project.max_group_size"
                 :validators="[make_min_validator(1)]"
                 input_style="max-width: 80px;"
               >
+                <template v-slot:label>
+                  Max group size
+                  <tooltip width="large" placement="top">
+                    When this is > 1, users will be prompted to register their
+                    group members the first time they visit the project page.
+                  </tooltip>
+                </template>
               </validated-int-input>
             </div>
           </div>
@@ -215,13 +218,13 @@
                 v-model="state.project.disallow_group_registration"
               />
               Disable group registration
+              <tooltip width="large" placement="top">
+                Temporarily prevent students registering new groups. Groups
+                already registered will be unaffected. <br />
+                NOTE: This will effectively prevent unregistered students from
+                submitting.
+              </tooltip>
             </label>
-            <tooltip width="large" placement="top">
-              Temporarily prevent students registering new groups. Groups
-              already registered will be unaffected. <br />
-              NOTE: This will effectively prevent unregistered students from
-              submitting.
-            </tooltip>
           </div>
         </fieldset>
       </div>
@@ -232,11 +235,11 @@
           <div class="form-field-wrapper">
             <label class="label" for="ultimate-submission-policy">
               Final graded submission policy
+              <tooltip width="large" placement="top">
+                Use students' most recent or best submission for their final
+                score.
+              </tooltip>
             </label>
-            <tooltip width="large" placement="top">
-              Use students' most recent or best submission for their final
-              score.
-            </tooltip>
             <div>
               <select
                 id="ultimate-submission-policy"
@@ -269,13 +272,13 @@
         <fieldset class="fieldset">
           <legend class="legend">Submission Limits</legend>
           <div class="form-field-wrapper">
-            <label class="label"> Submissions per day </label>
             <validated-nullable-int-input
-              id="submission-limit-per-day"
+              data-testid="submission_limit_per_day"
               v-model="state.project.submission_limit_per_day"
               :validators="[make_nullable_min_validator(1)]"
               input_style="max-width: 80px;"
             >
+              <template v-slot:label> Submissions per day </template>
             </validated-nullable-int-input>
           </div>
           <div class="checkbox-input-container">
@@ -292,18 +295,26 @@
           </div>
 
           <div class="form-field-wrapper">
-            <label class="label"> Reset submissions per day at: </label>
+            <label class="label" for="submission-limit-reset-time">
+              Reset submissions per day at:
+            </label>
             <div id="reset-time-picker-container">
               <input
                 type="time"
                 ref="submission_limit_reset_time_picker"
                 v-model="state.project.submission_limit_reset_time"
+                id="submission-limit-reset-time"
               />
               <span class="display-timezone">
                 {{ state.project.timezone }}
                 <i
                   class="edit-timezone-icon fas fa-pencil-alt"
+                  role="button"
+                  aria-label="edit timezone"
+                  tabindex="0"
                   @click="timezone_input.focus()"
+                  @keydown.space.prevent="timezone_input.focus()"
+                  @keydown.enter="timezone_input.focus()"
                 ></i>
               </span>
             </div>
@@ -319,24 +330,24 @@
                 :disabled="state.project.max_group_size === 1"
               />
               Groups get more submissions than individuals
+              <tooltip width="large" placement="top">
+                When unchecked, individuals and groups receive the same number
+                of submissions per day. When checked, the daily limit for a
+                group is multiplied by the number of users in that group. For
+                example, if the daily limit is 2, a group with 3 members would
+                receive 6 submissions per day with this box checked.
+              </tooltip>
             </label>
-            <tooltip width="large" placement="top">
-              When unchecked, individuals and groups receive the same number of
-              submissions per day. When checked, the daily limit for a group is
-              multiplied by the number of users in that group. For example, if
-              the daily limit is 2, a group with 3 members would receive 6
-              submissions per day with this box checked.
-            </tooltip>
           </div>
 
           <div class="form-field-wrapper">
-            <label class="label"> Bonus submissions per group </label>
             <validated-int-input
               ref="bonus_submissions_input"
               v-model="state.project.num_bonus_submissions"
               :validators="[make_min_validator(0)]"
               input_style="max-width: 80px;"
             >
+              <template v-slot:label> Bonus submissions per group </template>
             </validated-int-input>
           </div>
 
@@ -349,26 +360,26 @@
                 v-model="state.project.allow_late_days"
               />
               Allow late day tokens
+              <tooltip width="large" placement="top">
+                Whether students can use late day tokens for this project.
+              </tooltip>
             </label>
-            <tooltip width="medium" placement="top">
-              Whether students can use late day tokens for this project.
-            </tooltip>
           </div>
 
           <div class="form-field-wrapper">
-            <label for="total-submission-limit" class="label">
-              Total submission limit (Ever!)
-            </label>
-            <tooltip width="medium" placement="top">
-              A hard limit on how many times students can submit ever.
-            </tooltip>
             <validated-nullable-int-input
               ref="total_submissions_input"
-              id="total-submission-limit"
+              data-testid="total_submission_limit"
               v-model="state.project.total_submission_limit"
               :validators="[make_nullable_min_validator(1)]"
               input_style="max-width: 80px;"
             >
+              <template v-slot:label>
+                Total submission limit (Ever!)
+                <tooltip width="large" placement="top">
+                  A hard limit on how many times students can submit ever.
+                </tooltip>
+              </template>
             </validated-nullable-int-input>
           </div>
         </fieldset>
@@ -386,11 +397,11 @@
                 v-model="state.project.send_email_on_submission_received"
               />
               Send submission received email
+              <tooltip width="large" placement="top">
+                Students will receive a confirmation email when their submission
+                is recorded in the database.
+              </tooltip>
             </label>
-            <tooltip width="medium" placement="top">
-              Students will receive a confirmation email when their submission
-              is recorded in the database.
-            </tooltip>
           </div>
           <div class="checkbox-input-container">
             <label class="checkbox-label">
@@ -403,11 +414,11 @@
                 "
               />
               Send score summary email
+              <tooltip width="large" placement="top">
+                Students will receive a score-summary email when all
+                non-deferred test cases are finished grading.
+              </tooltip>
             </label>
-            <tooltip width="medium" placement="top">
-              Students will receive a score-summary email when all non-deferred
-              test cases are finished grading.
-            </tooltip>
           </div>
         </fieldset>
       </div>
@@ -424,21 +435,22 @@
                 v-model="state.project.use_honor_pledge"
               />
               Require honor pledge
+              <tooltip width="large" placement="top">
+                Students will be prompted to sign an honor pledge each time they
+                submit.
+              </tooltip>
             </label>
-            <tooltip width="medium" placement="top">
-              Students will be prompted to sign an honor pledge each time they
-              submit.
-            </tooltip>
           </div>
           <div class="form-field-wrapper" v-if="state.project.use_honor_pledge">
-            <label class="label">Honor pledge text</label>
             <validated-text-area-input
               data-testid="honor_pledge_text"
               v-model="state.project.honor_pledge_text"
               :validators="[]"
               :num_rows="4"
               input_style="max-width: 500px; width: 100%"
-            />
+            >
+              <template v-slot:label> Honor pledge text </template>
+            </validated-text-area-input>
           </div>
         </fieldset>
       </div>
@@ -464,7 +476,11 @@
       </div>
     </new-validated-form>
 
-    <div class="danger-zone-container">
+    <div
+      class="danger-zone-container"
+      role="region"
+      aria-label="delete project"
+    >
       <div class="delete-instructions">
         To delete this project, please delete all of its test cases first
         (regular test suites and mutation test suites).
