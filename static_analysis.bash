f4d96b8e14ecd9dfeedf002f51eb938317e827fd
Matt Mayfield <mattmayf@umich.edu>
2026-06-11 18:38:27 -0400

    Port context_menu_item.vue to <script setup> SFC

--- static_analysis.bash
+++ static_analysis.bash
@@ -12,6 +12,7 @@ lint_paths=(
     'src/components/CollapsibleContent.vue'
     'src/components/CollapsibleSection.vue'
     'src/components/context_menu/context_menu.vue'
+    'src/components/context_menu/context_menu_item.vue'
     'src/components/dropdown.vue'
     'src/components/group_members_form.vue'
     'src/components/last_saved.vue'
