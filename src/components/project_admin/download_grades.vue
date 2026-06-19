19e62d99569ef9dca699ee87e531e5f1f148ac2e
Matt Mayfield <mattmayf@umich.edu>
2026-06-19 15:11:40 -0400

    Accessibility updates for "Grades & Files" tab of project admin page (#595)
    * Add href attribute to links
    
    * Add download attribute to links
    
    * Add actual urls to href attribute of links

--- src/components/project_admin/download_grades.vue
+++ src/components/project_admin/download_grades.vue
@@ -93,8 +93,12 @@
           <div class="started-at">{{format_datetime_short(download.created_at)}}</div>
           <div class="file">
             <a v-if="download.progress === 100 && download.error_msg.length === 0"
-               @click="download_task_result(download)"
-               class="file-name">{{get_filename(download)}}
+               :href="`/api/download_tasks/${download.pk}/result/`"
+               @click.prevent="download_task_result(download)"
+               class="file-name"
+               download
+            >
+              {{get_filename(download)}}
             </a>
             <template v-if="download.error_msg.length !== 0">ERROR</template>
           </div>
@@ -375,7 +379,7 @@ $progress-column-width: 20%;
 }
 
 .file-name {
-  color: $ocean-blue;
+  color: darken($ocean-blue, 10%);
   word-break: break-word;
 }
 
