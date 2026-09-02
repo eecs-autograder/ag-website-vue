963132a7de610475edbf07bf1c64df00fc7ffc86
Matt Mayfield <mattmayf@umich.edu>
2026-09-02 13:39:54 -0400

    Port group_summary_panel.vue to <script setup> SFC
    Fix name-based GroupSummaryPanel lookups in test_handgrading_container.ts
    to find by component reference instead.

--- src/components/project_view/handgrading/group_summary_panel.vue
+++ src/components/project_view/handgrading/group_summary_panel.vue
@@ -1,56 +1,60 @@
 <template>
-  <div class="group-summary-panel"
-       :class="{
-         'graded': handgrading_status === HandgradingStatus.graded,
-         'ungraded': handgrading_status === HandgradingStatus.ungraded,
-         'in-progress': handgrading_status === HandgradingStatus.in_progress,
-         'no-handgradeable-submission': (
-            handgrading_status === HandgradingStatus.no_handgradeable_submission
-          ),
-       }"
-       v-on="$listeners">
+  <div
+    class="group-summary-panel"
+    :class="{
+      graded: handgrading_status === HandgradingStatus.graded,
+      ungraded: handgrading_status === HandgradingStatus.ungraded,
+      'in-progress': handgrading_status === HandgradingStatus.in_progress,
+      'no-handgradeable-submission':
+        handgrading_status === HandgradingStatus.no_handgradeable_submission,
+    }"
+    v-on="$listeners"
+  >
     <div class="member-names">
-      <div class="member-name" v-for="member of group_summary.member_names" :key="member">
-        {{member}}
+      <div
+        class="member-name"
+        v-for="member of group_summary.member_names"
+        :key="member"
+      >
+        {{ member }}
       </div>
     </div>
     <div class="status">
-      {{status_text}}
+      {{ status_text }}
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script setup lang="ts">
+import { computed } from "vue";
 
-import * as ag_cli from 'ag-client-typescript';
+import * as ag_cli from "ag-client-typescript";
 
-import { get_handgrading_status, HandgradingStatus } from './handgrading_status';
+import {
+  get_handgrading_status,
+  HandgradingStatus,
+} from "./handgrading_status";
 
-@Component({})
-export default class GroupSummaryPanel extends Vue {
-  @Prop({required: true})
-  group_summary!: ag_cli.GroupWithHandgradingResultSummary;
+const props = defineProps<{
+  group_summary: ag_cli.GroupWithHandgradingResultSummary;
+}>();
 
-  readonly HandgradingStatus = HandgradingStatus;
+const handgrading_status = computed(() =>
+  get_handgrading_status(props.group_summary),
+);
 
-  get handgrading_status() {
-    return get_handgrading_status(this.group_summary);
+const status_text = computed(() => {
+  let status = get_handgrading_status(props.group_summary);
+  if (status === HandgradingStatus.graded) {
+    let result = props.group_summary.handgrading_result!;
+    return `${result.total_points}/${result.total_points_possible}`;
   }
-
-  get status_text() {
-    let status = get_handgrading_status(this.group_summary);
-    if (status === HandgradingStatus.graded) {
-      let result = this.group_summary.handgrading_result!;
-      return `${result.total_points}/${result.total_points_possible}`;
-    }
-    return status;
-  }
-}
+  return status;
+});
 </script>
 
 <style scoped lang="scss">
-@import '@/styles/colors.scss';
+@import "@/styles/colors.scss";
 
 * {
   box-sizing: border-box;
@@ -61,15 +65,16 @@ export default class GroupSummaryPanel extends Vue {
 .group-summary-panel {
   display: flex;
   justify-content: space-between;
-  font-size: .875rem;
+  font-size: 0.875rem;
 }
 
-.member-names, .status {
-  padding: .25rem .375rem;
+.member-names,
+.status {
+  padding: 0.25rem 0.375rem;
 }
 
 .member-name {
-  padding: .125rem 0;
+  padding: 0.125rem 0;
 }
 
 .graded {
@@ -87,5 +92,4 @@ export default class GroupSummaryPanel extends Vue {
 .no-handgradeable-submission {
   color: $normal-text-color-3;
 }
-
 </style>
