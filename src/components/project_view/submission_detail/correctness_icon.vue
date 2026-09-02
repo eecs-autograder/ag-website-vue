ea6b56bb5e30a8b55706bb317ed842bd17e82d24
Matt Mayfield <mattmayf@umich.edu>
2026-09-02 13:38:08 -0400

    Port correctness_icon.vue to <script setup> SFC

--- src/components/project_view/submission_detail/correctness_icon.vue
+++ src/components/project_view/submission_detail/correctness_icon.vue
@@ -1,42 +1,41 @@
 <template>
   <div id="correctness-icon">
-    <div v-if="correctness_level === CorrectnessLevel.all_correct"
-         class="correctness-level all-correct">
+    <div
+      v-if="correctness_level === CorrectnessLevel.all_correct"
+      class="correctness-level all-correct"
+    >
       <i class="fas fa-check" role="img" aria-label="Correct"></i>
     </div>
-    <div v-else-if="correctness_level === CorrectnessLevel.some_correct"
-         class="correctness-level some-correct">
+    <div
+      v-else-if="correctness_level === CorrectnessLevel.some_correct"
+      class="correctness-level some-correct"
+    >
       <i class="fas fa-times" role="img" aria-label="Partially correct"></i>
     </div>
-    <div v-else-if="correctness_level === CorrectnessLevel.none_correct"
-         class="correctness-level none-correct">
+    <div
+      v-else-if="correctness_level === CorrectnessLevel.none_correct"
+      class="correctness-level none-correct"
+    >
       <i class="fas fa-times" role="img" aria-label="Incorrect"></i>
     </div>
-    <div v-else-if="correctness_level === CorrectnessLevel.info_only"
-         class="correctness-level info-only">
-    </div>
-    <div v-else
-         class="correctness-level not-available">
-    </div>
+    <div
+      v-else-if="correctness_level === CorrectnessLevel.info_only"
+      class="correctness-level info-only"
+    ></div>
+    <div v-else class="correctness-level not-available"></div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script setup lang="ts">
+import { CorrectnessLevel } from "@/components/project_view/submission_detail/correctness";
 
-import { CorrectnessLevel } from '@/components/project_view/submission_detail/correctness';
-
-@Component
-export default class CorrectnessIcon extends Vue {
-  @Prop({required: true, type: String})
-  correctness_level!: string;
-
-  readonly CorrectnessLevel = CorrectnessLevel;
-}
+defineProps<{
+  correctness_level: string;
+}>();
 </script>
 
 <style scoped lang="scss">
-@import '@/styles/colors.scss';
+@import "@/styles/colors.scss";
 
 #correctness-icon {
   padding: 0 5px;
@@ -46,5 +45,4 @@ export default class CorrectnessIcon extends Vue {
 .correctness-level {
   display: inline-block;
 }
-
 </style>
