4637a7627707ba2ef58be6060630014fadf41c8a
Matt Mayfield <mattmayf@umich.edu>
2026-09-03 13:39:04 -0400

    Port single_course.vue to <script setup> SFC

--- static_analysis.bash
+++ static_analysis.bash
@@ -13,6 +13,7 @@ lint_paths=(
     'src/components/CollapsibleSection.vue'
     'src/components/context_menu/context_menu.vue'
     'src/components/context_menu/context_menu_item.vue'
+    'src/components/course_list/single_course.vue'
     'src/components/dropdown.vue'
     'src/components/group_members_form.vue'
     'src/components/last_saved.vue'
