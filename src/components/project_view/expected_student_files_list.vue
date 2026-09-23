775d2c7b471db2628c6854bc49cd970441d9790a
Matt Mayfield <mattmayf@umich.edu>
2026-09-23 13:39:24 -0400

    Port expected_student_files_list.vue to <script setup> SFC (#607)
    * Port expected_student_files_list.vue to <script setup> SFC
    
    * Make expected files disclosure a real button

--- src/components/project_view/expected_student_files_list.vue
+++ src/components/project_view/expected_student_files_list.vue
@@ -1,58 +1,63 @@
 <template>
   <div class="expected-student-files-list">
-    <div
-      class="show-all"
-      role="button"
-      tabindex="0"
+    <button
+      type="button"
+      class="show-all unstyled-button"
+      :aria-expanded="show"
       aria-controls="expected-files-to-submit-list"
-      @click="d_show = !d_show"
-      @keydown.enter="d_show = !d_show"
-      @keydown.space.prevent="d_show = !d_show"
-    >What files should I submit?</div>
-    <ul id="expected-files-to-submit-list" class="file-list" v-show="d_show">
-      <li v-for="item of expected_student_files" :key="item.pk" class="file-list-item">
-        - <span class="filename">{{item.pattern}}</span>
-        <span class="num-matches" v-if="item.min_num_matches !== 1 || item.max_num_matches !== 1">
-          ({{item.min_num_matches}}-{{item.max_num_matches}} matches)
+      @click="show = !show"
+    >
+      What files should I submit?
+    </button>
+    <ul id="expected-files-to-submit-list" class="file-list" v-show="show">
+      <li
+        v-for="item of expected_student_files"
+        :key="item.pk"
+        class="file-list-item"
+      >
+        - <span class="filename">{{ item.pattern }}</span>
+        <span
+          class="num-matches"
+          v-if="item.min_num_matches !== 1 || item.max_num_matches !== 1"
+        >
+          ({{ item.min_num_matches }}-{{ item.max_num_matches }} matches)
         </span>
       </li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script setup lang="ts">
+import { ref } from "vue";
 
-import { ExpectedStudentFile } from 'ag-client-typescript';
+import { ExpectedStudentFile } from "ag-client-typescript";
 
-@Component
-export default class ExpectedStudentFilesList extends Vue {
-  @Prop({required: true, type: Array})
-  expected_student_files!: ExpectedStudentFile[];
+defineProps<{
+  expected_student_files: ExpectedStudentFile[];
+}>();
 
-  d_show = false;
-}
+const show = ref(false);
 </script>
 
 <style scoped lang="scss">
-@import '@/styles/colors.scss';
+@import "@/styles/button_styles.scss";
+@import "@/styles/colors.scss";
 
 * {
   box-sizing: border-box;
 }
 
-
 .expected-student-files-list {
-  margin: .875rem 0;
+  margin: 0.875rem 0;
 }
 
 .file-list {
-  margin: .25rem 0;
+  margin: 0.25rem 0;
   padding-inline-start: 1rem;
 }
 
 .file-list-item {
-  font-size: .95em;
+  font-size: 0.95em;
   display: block;
 }
 
@@ -65,12 +70,10 @@ export default class ExpectedStudentFilesList extends Vue {
 }
 
 .show-all {
-  cursor: pointer;
   color: darken($ocean-blue, 10%);
 
   &:hover {
     color: darken($ocean-blue, 20%);
   }
 }
-
 </style>
